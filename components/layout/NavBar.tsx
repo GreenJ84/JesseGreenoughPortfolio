15efc1851ef7dfa1f056661cf072f0e923f13d6b
Jesse Greenough <jesse.greenough84@gmail.com>
2022-11-29 19:06:35 -0800

    Added scrolling updated styles

--- components/layout/NavBar.tsx
+++ components/layout/NavBar.tsx
@@ -1,5 +1,6 @@
-import React, { useState } from "react";
-import useRouter from "next";
+import React, { useEffect, useState } from "react";
+import Image from "next/image";
+import Link from "next/link";
 
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
@@ -13,72 +14,87 @@ import { AiFillStar, AiOutlineHome, AiOutlineFundProjectionScreen, AiOutlineUser
 import { CgFileDocument } from "react-icons/cg";
 
 import logo from "../../public/assets/logo.png";
-import Image from "next/image";
-import Link from "next/link";
+import { useRouter } from "next/router";
+
+const css = require('./NavBar.module.css');
 
 const NavBar = () => {
     const [expandBar, setExpandBar] = useState(false);
     const [navFade, setNavFade] = useState(false);
+    const { pathname } = useRouter();
 
-    const scrollHandler = () => {
-        if (window.scrollY >= 20) {
-            setNavFade(true);
-        } else {
-            setNavFade(false);
+    useEffect(() => {
+        window.scrollTo(0, 0);
+    }, [pathname])
+
+    useEffect(() => {
+        const scrollHandler = () => {
+            if (window.scrollY >= 20) {
+                setNavFade(true);
+            } else {
+                setNavFade(false);
+            }
         }
-    }
+        window.addEventListener("scroll", scrollHandler);
+
+        const unMount = () => {
+            window.removeEventListener("scroll", scrollHandler);
+        return unMount
+        };
+    });
 
 
     return (
-        <Navbar expanded={expandBar} fixed="top" expand="md" className={navFade ? "sticky" : "navbar"}>
+        <Navbar expanded={expandBar} fixed="top" expand="md" className={navFade ? css.sticky : css.navbar}>
             <Container>
-                <Navbar.Brand href="/" className="d-flex">
-                    <Image src={logo} className="img-fluid logo" alt="brand" />
+                <Navbar.Brand href="/" className={ css.navbarBrand && "d-flex"}>
+                    <Image src={logo} className={css.logo && "img-fluid"} alt="brand" />
                 </Navbar.Brand>
-                <Navbar.Toggle aria-controls="responsive-navbar-nav" onClick={() => { setExpandBar(expandBar ? false : true);}}>
+                <Navbar.Toggle className={ css.navbarToggler } aria-controls="responsive-navbar-nav" onClick={() => { setExpandBar(expandBar ? false : true);}}>
                     <span></span>
                     <span></span>
                     <span></span>
                 </Navbar.Toggle>
                 <Navbar.Collapse id="responsive-navbar-nav">
-                    <Nav className="ms-auto" defaultActiveKey="#home">
-                        <Nav.Item>
+                    <Nav className={ css.navbarNav && "ms-auto"} defaultActiveKey="#home">
+                        <Nav.Item className={ css.navItem }>
                             <Nav.Link as={Link} href='/' onClick={() => setExpandBar(false)}>
                                 <AiOutlineHome style={{ marginBottom: "2px" }} />
                                 Home
                             </Nav.Link>
                         </Nav.Item>
 
-                    <Nav.Item>
+                    <Nav.Item className={ css.navItem }>
                         <Nav.Link as={Link} href="/about" onClick={() => setExpandBar(false)}>
                             <AiOutlineUser style={{ marginBottom: "2px" }} />
                             About
                         </Nav.Link>
                     </Nav.Item>
 
-                    <Nav.Item>
-                        <Nav.Link as={Link} href="/project" onClick={() => setExpandBar(false)}>
+                    <Nav.Item className={ css.navItem }>
+                        <Nav.Link as={Link} href="/projects" onClick={() => setExpandBar(false)}>
                             <AiOutlineFundProjectionScreen style={{ marginBottom: "2px" }} />
                             {" "}
                             Projects
                         </Nav.Link>
+                        </Nav.Item>
+                        
+                    <Nav.Item className={ css.navItem }>
+                        <Nav.Link href="/experience" rel="noreferrer" >
+                            <ImBlog style={{ marginBottom: "2px" }} />
+                            Expeience
+                        </Nav.Link>
                     </Nav.Item>
 
-                    <Nav.Item>
+                    <Nav.Item className={ css.navItem }>
                         <Nav.Link as={Link} href="/resume" onClick={() => setExpandBar(false)}>
                             <CgFileDocument style={{ marginBottom: "2px" }} />
                             Resume
                         </Nav.Link>
                     </Nav.Item>
 
-                    <Nav.Item>
-                        <Nav.Link href="https://blogs.soumya-jit.tech/" target="_blank" rel="noreferrer" >
-                            <ImBlog style={{ marginBottom: "2px" }} />
-                            Blogs
-                        </Nav.Link>
-                    </Nav.Item>
 
-                    <Nav.Item className="fork-btn">
+                    <Nav.Item className={ css.navItem && "fork-btn"}>
                         <Button href="https://github.com/GreenJ84" target="_blank" className="github-btn-inner">
                             <CgGitFork style={{ fontSize: "1.2em" }} />{" "}
                             <AiFillStar style={{ fontSize: "1.1em" }} />
