b7b5d8d3f2358264a5f3b48491e5c9319b7c14ab
Jesse Greenough <jesse.greenough84@gmail.com>
2022-11-29 18:58:31 -0800

    Added css to apps main file

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,26 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { useEffect, useState } from 'react';
+
+import Preloader from '../components/layout/Preloader';
+const css = require('./App.module.css')
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const [load, setLoad] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoad(false);
+    }, 1200);
+
+    return () => clearTimeout(timer);
+  }, []);
+  
+  return (
+    <>
+      <Preloader load={load} />
+      <div className="App" id={load ? "no-scroll" : "scroll"}>
+          <Component {...pageProps} />
+      </div>
+    </>)
 }
