d6f50b7a070e802556d14a3d47e9d470ac00b64b
Jesse Greenough <jesse.greenough84@gmail.com>
2022-11-29 19:24:22 -0800

    updated app to render NavBar and Footer

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,8 @@ import type { AppProps } from 'next/app'
 import { useEffect, useState } from 'react';
 
 import Preloader from '../components/layout/Preloader';
+import NavBar from '../components/layout/NavBar';
+import Footer from '../components/layout/Footer';
 const css = require('./App.module.css')
 
 export default function App({ Component, pageProps }: AppProps) {
@@ -15,12 +17,14 @@ export default function App({ Component, pageProps }: AppProps) {
 
     return () => clearTimeout(timer);
   }, []);
-  
+
   return (
     <>
       <Preloader load={load} />
-      <div className="App" id={load ? "no-scroll" : "scroll"}>
+      <div className="App" id={load ? css.noScroll : "scroll"}>
+        <NavBar />
           <Component {...pageProps} />
+        <Footer />
       </div>
     </>)
 }
