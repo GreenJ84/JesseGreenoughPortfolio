3bff149eeb1c1fd2f9df5e87c2adaf6562f21102
Jesse Greenough <jesse.greenough84@gmail.com>
2022-11-30 23:08:29 -0800

    Added components to home index

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,13 @@
+import HomeBottom from "../components/HomePage/HomeBottom";
+import HomeTop from "../components/HomePage/HomeTop";
+
 
 const HomePage = () => {
   return (
-    <>
-    
-    </>
+    <section>
+      <HomeTop />
+      <HomeBottom />
+    </section>
   );
 }
 
