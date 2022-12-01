9d42b114077d1fd1732ea8647b04a294b9bd9af8
Jesse Greenough <jesse.greenough84@gmail.com>
2022-11-30 16:31:21 -0800

    First finalization of Navbar and styling

--- components/layout/NavBar.tsx
+++ components/layout/NavBar.tsx
@@ -20,6 +20,7 @@ const css = require('./NavBar.module.css');
 
 const NavBar = () => {
     const [expandBar, setExpandBar] = useState(false);
+    const [wideScreen, setWideScreen] = useState(true)
     const [navFade, setNavFade] = useState(false);
     const { pathname } = useRouter();
 
@@ -29,13 +30,23 @@ const NavBar = () => {
 
     useEffect(() => {
         const scrollHandler = () => {
-            if (window.scrollY >= 20) {
+            if (window.scrollY >= 60) {
                 setNavFade(true);
             } else {
                 setNavFade(false);
             }
         }
+        const sizeHandler = () => {
+            if (innerWidth > 900) {
+                setExpandBar(true);
+                setWideScreen(true);
+            } else {
+                setWideScreen(false);
+            }
+        }
         window.addEventListener("scroll", scrollHandler);
+        window.addEventListener('resize',
+        sizeHandler)
 
         const unMount = () => {
             window.removeEventListener("scroll", scrollHandler);
@@ -43,66 +54,65 @@ const NavBar = () => {
         };
     });
 
-
     return (
         <Navbar expanded={expandBar} fixed="top" expand="md" className={navFade ? css.sticky : css.navbar}>
-            <Container>
-                <Navbar.Brand href="/" className={ css.navbarBrand && "d-flex"}>
-                    <Image src={logo} className={css.logo && "img-fluid"} alt="brand" />
+            <Container className={ css.navbarContainer }>
+                <Navbar.Brand href="/" className={ css.navBarBrand }>
+                    <Image src={logo} className={ css.logo } alt="brand" />
                 </Navbar.Brand>
-                <Navbar.Toggle className={ css.navbarToggler } aria-controls="responsive-navbar-nav" onClick={() => { setExpandBar(expandBar ? false : true);}}>
-                    <span></span>
-                    <span></span>
-                    <span></span>
-                </Navbar.Toggle>
                 <Navbar.Collapse id="responsive-navbar-nav">
-                    <Nav className={ css.navbarNav && "ms-auto"} defaultActiveKey="#home">
+                    <Nav className={ wideScreen ? css.navbarNav : expandBar ? css.navbarNav : css.navClosed }defaultActiveKey="#home">
                         <Nav.Item className={ css.navItem }>
-                            <Nav.Link as={Link} href='/' onClick={() => setExpandBar(false)}>
+                            <Nav.Link as={Link} href='/' className={ css.navLink } onClick={() => setExpandBar(false)}>
                                 <AiOutlineHome style={{ marginBottom: "2px" }} />
                                 Home
                             </Nav.Link>
                         </Nav.Item>
 
-                    <Nav.Item className={ css.navItem }>
-                        <Nav.Link as={Link} href="/about" onClick={() => setExpandBar(false)}>
-                            <AiOutlineUser style={{ marginBottom: "2px" }} />
-                            About
-                        </Nav.Link>
-                    </Nav.Item>
-
-                    <Nav.Item className={ css.navItem }>
-                        <Nav.Link as={Link} href="/projects" onClick={() => setExpandBar(false)}>
-                            <AiOutlineFundProjectionScreen style={{ marginBottom: "2px" }} />
-                            {" "}
-                            Projects
-                        </Nav.Link>
+                        <Nav.Item className={ css.navItem }>
+                            <Nav.Link as={Link} href="/about" className={ css.navLink } onClick={() => setExpandBar(false)}>
+                                <AiOutlineUser style={{ marginBottom: "2px" }} />
+                                About
+                            </Nav.Link>
                         </Nav.Item>
-                        
-                    <Nav.Item className={ css.navItem }>
-                        <Nav.Link href="/experience" rel="noreferrer" >
-                            <ImBlog style={{ marginBottom: "2px" }} />
-                            Expeience
-                        </Nav.Link>
-                    </Nav.Item>
 
-                    <Nav.Item className={ css.navItem }>
-                        <Nav.Link as={Link} href="/resume" onClick={() => setExpandBar(false)}>
-                            <CgFileDocument style={{ marginBottom: "2px" }} />
-                            Resume
-                        </Nav.Link>
-                    </Nav.Item>
+                        <Nav.Item className={ css.navItem }>
+                            <Nav.Link as={Link} href="/projects" className={ css.navLink }  onClick={() => setExpandBar(false)}>
+                                <AiOutlineFundProjectionScreen style={{ marginBottom: "2px" }} />
+                                {" "}
+                                Projects
+                            </Nav.Link>
+                            </Nav.Item>
+                            
+                        <Nav.Item className={ css.navItem }>
+                            <Nav.Link href="/experience" className={ css.navLink } rel="noreferrer" >
+                                <ImBlog style={{ marginBottom: "2px" }} />
+                                Expeience
+                            </Nav.Link>
+                        </Nav.Item>
+
+                        <Nav.Item className={ css.navItem }>
+                            <Nav.Link as={Link} href="/resume" className={ css.navLink } onClick={() => setExpandBar(false)}>
+                                <CgFileDocument style={{ marginBottom: "2px" }} />
+                                Resume
+                            </Nav.Link>
+                        </Nav.Item>
 
 
-                    <Nav.Item className={ css.navItem && "fork-btn"}>
-                        <Button href="https://github.com/GreenJ84" target="_blank" className="github-btn-inner">
-                            <CgGitFork style={{ fontSize: "1.2em" }} />{" "}
-                            <AiFillStar style={{ fontSize: "1.1em" }} />
-                        </Button>
-                    </Nav.Item>
+                        { wideScreen && <Nav.Item className={ css.navItem && "fork-btn"}>
+                            <Button href="https://github.com/GreenJ84" target="_blank" className="github-btn-inner">
+                                <CgGitFork style={{ fontSize: "1.2em" }} />{" "}
+                                <AiFillStar style={{ fontSize: "1.1em" }} />
+                            </Button>
+                        </Nav.Item>}
 
                     </Nav>
                 </Navbar.Collapse>
+                <Navbar.Toggle className={ expandBar ? css.navbarToggler : css.navbarTogglerClosed } aria-controls="responsive-navbar-nav" onClick={() => { setExpandBar(expandBar ? false : true); console.log(expandBar)}}>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </Navbar.Toggle>
             </Container>
         </Navbar>
     )
