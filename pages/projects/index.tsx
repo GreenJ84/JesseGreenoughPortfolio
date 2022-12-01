d27dddabc4c9452201d681f4e1660a304976a718
Jesse Greenough <jesse.greenough84@gmail.com>
2022-12-01 12:21:00 -0800

    Started Projects Page with components

--- pages/projects/index.tsx
+++ pages/projects/index.tsx
@@ -1,8 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Container } from 'react-bootstrap'
+import ProjectNavbar from '../../components/ProjectsPage/ProjectNavbar';
+import data from '../../Utils/data/ProjectData'
+
+export type Category = "react" | "python" | "java";
+export interface IProject {
+    name: string;
+    description: string;
+    image_path: string;
+    deployed_url: string | null;
+    github_url: string;
+    category: Category[];
+    key_techs: string[];
+}
 
 const ProjectPage = () => {
+    const [projectData, setProjectData] = useState( data )
+
     return (
-        <div>ProjectPage</div>
+        <Container fluid>
+            <>
+            <ProjectNavbar />
+            {data.map((project: IProject) => {
+                <
+            })}
+            </>
+        </Container>
     );
 }
 
