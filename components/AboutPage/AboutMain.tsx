fb57be96425a2ade9672e118829811b48f6a7f3e
Jesse Greenough <jesse.greenough84@gmail.com>
2022-12-01 02:55:54 -0800

    Moved components and their styles out

--- components/AboutPage/AboutMain.tsx
+++ components/AboutPage/AboutMain.tsx
@@ -1,12 +1,9 @@
 import Image from 'next/image'
 import React from 'react'
-import { Col, Container, Row } from 'react-bootstrap'
+import { Container } from 'react-bootstrap'
 
 import AboutCard from './AboutCard'
-import DeveloperTools from './DeveloperTools'
-import TechnicalSkills from './TechnicalSkills'
 import laptopImage from '../../public/assets/about.png'
-import GithubCard from './githubCard'
 
 const css = require('./AboutMain.module.css')
 
@@ -18,20 +15,7 @@ const AboutMain = () => {
             </h1>
             <AboutCard />
             <Image src={ laptopImage } alt="about" className={ css.image } />
-            <h1 className={ css.projectHeading }>
-                Technical <strong className="purple">Skillset </strong>
-            </h1>
-            <TechnicalSkills />
 
-            <h1 className={ css.projectHeading }>
-                <strong className="purple">Tools</strong> I use
-            </h1>
-            <DeveloperTools />
-
-            <h1 className={ css.projectHeading } >
-                Days I <strong className="purple">Code</strong>
-            </h1>
-            <GithubCard />
         </Container>
     )
 }
