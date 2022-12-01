fe125a56211286f149e0efe24b98bcc462028b26
Jesse Greenough <jesse.greenough84@gmail.com>
2022-12-01 03:18:05 -0800

    Added New AboutInfo component

--- pages/about/index.tsx
+++ pages/about/index.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import { Container } from 'react-bootstrap';
+import AboutInfo from '../../components/AboutPage/AboutInfo';
 import AboutMain from '../../components/AboutPage/AboutMain';
 
 const AboutPage = () => {
     return (
         <Container fluid className="about-section">
             <AboutMain />
+            <AboutInfo />
         </Container>
     )
 }
