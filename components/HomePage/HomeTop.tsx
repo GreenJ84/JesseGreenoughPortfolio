d11026e92f2d89790d7b68c0be99346e18cc4162
Jesse Greenough <jesse.greenough84@gmail.com>
2022-11-30 23:07:51 -0800

    Updated HomeTop styling

--- components/HomePage/HomeTop.tsx
+++ components/HomePage/HomeTop.tsx
@@ -9,35 +9,49 @@ const css = require('./HomeTop.module.css')
 
 const HomeTop = () => {
     return (
-        <Container fluid className={css.homeSection} id="home">
-        <Particle />
-        <Container className={ css.homeContent }>
-        <Row>
-            <Col md={7} className={ css.homeHeader }>
-                <h1 style={{ paddingBottom: 15 }} className={ css.heading }>
-                    Hi There!{" "}
-                <span className={ css.wave } role="img" aria-labelledby="wave">
-                👋🏻
-                </span>
-            </h1>
+        <Container fluid className={ css.homeSection } id="home">
+            <Particle />
+            <Container className={ css.homeContent }>
+                <Row>
+                    <Col md={7} className={ css.homeHeader }>
+                        <h1 className={ css.heading }>
+                            Hi There!{" "}
+                            <span className={ css.wave } role="img" aria-labelledby="wave">
+                            👋🏻
+                            </span>
+                        </h1>
 
-            <h1 className={ css.headingName }>
-                I&apos;M
-                <strong className={ css.mainName }> SOUMYAJIT BEHERA</strong>
-            </h1>
+                        <h1 className={ css.headingName }>
+                            I&apos;M
+                            <strong className={ css.mainName }> JESSE GREENOUGH </strong>
+                        </h1>
 
-            <div style={{ padding: 50, textAlign: "left" }}>
-                <TypeWrite />
-            </div>
-            </Col>
+                        <div className={ css.typeText }>
+                            <TypeWrite typePrompts={props1} />
+                            <br/>
+                            <TypeWrite typePrompts={ props2 }/>
+                        </div>
+                    </Col>
 
-            <Col md={5} style={{ paddingBottom: 20 }}>
-            <Image src={homeLogo} alt="home pic" className="img-fluid" style={{ maxHeight: "450px" }} />
-            </Col>
-        </Row>
+                    <Col md={5} style={{ paddingBottom: 20 }}>
+                        <Image src={homeLogo} alt="home pic" className="img-fluid" style={{ maxHeight: "450px" }} />
+                    </Col>
+                </Row>
+            </Container>
         </Container>
-    </Container>
     )
 }
 
 export default HomeTop
+
+const props1 = [
+    "Full-stack Developer",
+    "TypeScript React focused Software Engineer",
+    "Open Source Contributor",
+]
+
+const props2 = [
+    "Front/Back-end Capable",
+    "SQL/NoSQL Database, Python, & Java skills",
+    "Always actively learning",
+]
