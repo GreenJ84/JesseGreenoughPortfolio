f7afc71d06b00bd9f7d3caa137fb4a79d119e171
Jesse Greenough <jesse.greenough84@gmail.com>
2022-12-01 22:00:09 -0800

    Created navItem component

---
+++ components/ProjectsPage/NavItem.tsx
@@ -0,0 +1,29 @@
+import React, { useEffect, useState } from 'react'
+import { Category } from '../../pages/projects'
+import { ProjectNavProps } from './ProjectNavbar'
+
+const css = require('./NavItem.module.css')
+
+interface navItemProps extends ProjectNavProps{
+    value: Category | "all"
+}
+
+const NavItem = (props: navItemProps) => {
+    const [isActive, setIsActive] = useState(false)
+    const display = props.value.charAt(0).toUpperCase() + props.value.slice(1);
+    
+    useEffect(() => {
+        if (props.active === props.value) {
+            setIsActive(true)
+        }
+        else { setIsActive(false) }
+    }, [props.active, props.value])
+
+    return (
+        <li className={ isActive ? css.activeNav : css.navItem } onClick={() => props.filterHandler(props.value)}>
+            { display }
+        </li>
+    )
+}
+
+export default NavItem
