d339d9db001680fd2dce49e7ee1797851facdbc1
Jesse Greenough <jesse.greenough84@gmail.com>
2022-12-02 00:46:33 -0800

    Set EduExp layout with components

---
+++ components/ExperiencePage/Education/EduExp.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Certifications from './Certifications'
+import Degree from './Degree'
+import EduBody from './EduBody'
+
+const EduExp = () => {
+    return (
+        <>
+            <EduBody />
+            <Degree />
+            <Certifications />
+        </>
+    )
+}
+
+export default EduExp
