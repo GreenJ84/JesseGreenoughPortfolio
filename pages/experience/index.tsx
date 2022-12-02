668f14d09055788cdfa95b1c117e50f7e5e3b7ee
Jesse Greenough <jesse.greenough84@gmail.com>
2022-12-02 00:34:47 -0800

    Added display change function

--- pages/experience/index.tsx
+++ pages/experience/index.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import { Container } from 'react-bootstrap';
 import EduExp from '../../components/ExperiencePage/eduExp';
 import ExpNavbar from '../../components/ExperiencePage/ExpNavbar';
 import WorkExp from '../../components/ExperiencePage/workExp';
@@ -6,17 +7,24 @@ import WorkExp from '../../components/ExperiencePage/workExp';
 const ExperiencePage = () => {
     const [showWork, setShowWork] = useState(false);
 
+    const changeActive = () => {
+        if (showWork) {
+            setShowWork(false)
+        } else {
+            setShowWork(true)
+        }
+    }
 
     return (
-        <>
-            <ExpNavbar />
+        <Container style={{ padding: '12rem 1rem 1rem'}}>
+            <ExpNavbar changeActive={ changeActive }  workActive={ showWork } />
             <div>
                 { showWork ? 
                     <WorkExp /> :
                     <EduExp />
                 }
             </div>
-        </>
+        </Container>
     )
 }
 
