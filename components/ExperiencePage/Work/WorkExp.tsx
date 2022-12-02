51bdbd24164f78ba253e6578b384ef1f17133e25
Jesse Greenough <jesse.greenough84@gmail.com>
2022-12-02 15:56:22 -0800

    Created Work main layout

--- components/ExperiencePage/Work/WorkExp.tsx
+++ components/ExperiencePage/Work/WorkExp.tsx
@@ -1,8 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { workHistory } from "../../../Utils/data/WorkData"
+import WorkBody from './WorkBody';
+import WorkCard from './WorkCard';
 
 const WorkExp = () => {
+    const [workData, setWorkData] = useState(workHistory);
+
     return (
-        <div>WorkExp</div>
+        <>
+            <WorkBody />
+            <div>
+                {workData.map((item) => <WorkCard key={item.position} work={item} />)}
+            </div>
+        </>
     )
 }
 
