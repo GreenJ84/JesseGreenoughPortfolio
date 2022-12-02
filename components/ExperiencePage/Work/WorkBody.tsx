ecf07da465fe36bc1580f69182e48be2bb4e5955
Jesse Greenough <jesse.greenough84@gmail.com>
2022-12-02 15:56:51 -0800

    Created template work components

---
+++ components/ExperiencePage/Work/WorkBody.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const WorkBody = () => {
+  return (
+    <div>WorkBody</div>
+  )
+}
+
+export default WorkBody
---
+++ components/ExperiencePage/Work/WorkCard.tsx
@@ -0,0 +1,16 @@
+import React from "react"
+import { workItem } from "../../../Utils/data/WorkData"
+
+interface workCard{
+    work: workItem
+}
+
+const WorkCard = ( prop: workCard ) => {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default WorkCard
