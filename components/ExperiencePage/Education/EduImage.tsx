5002f3cdf996e2587d40d2b9baaccdda4f4f01f1
Jesse Greenough <jesse.greenough84@gmail.com>
2022-12-02 01:31:48 -0800

    Changed svg sizings

--- components/ExperiencePage/Education/EduImage.tsx
+++ components/ExperiencePage/Education/EduImage.tsx
@@ -7,7 +7,7 @@ export default function EducationImg() {
       data-name="Layer 1"
       xmlns="http://www.w3.org/2000/svg"
       // xmlns:xlink="http://www.w3.org/1999/xlink"
-      width="795.39431"
+      width="655.39431"
       height="574.03802"
       viewBox="0 0 795.39431 574.03802"
     >
