6db417c1f72144ab6faa05faa7890f68358b9801
Jesse Greenough <jesse.greenough84@gmail.com>
2022-12-02 13:19:04 -0800

    Added visit button to DegreeCard

--- components/ExperiencePage/Education/DegreeCard.tsx
+++ components/ExperiencePage/Education/DegreeCard.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { educationType } from '../../../Utils/data/EducationData'
+import { HiOutlineExternalLink } from 'react-icons/hi'
 
 const css = require('./DegreeCard.module.css')
 
@@ -10,7 +11,7 @@ interface degreeCard {
 const DegreeCard = (props: degreeCard ) => {
     return (
         <div className={css.cardBody}>
-            <div>
+            <div className={ css.cardHead }>
                 <h1>
                     {props.education.college}
                 </h1>
@@ -20,9 +21,11 @@ const DegreeCard = (props: degreeCard ) => {
                 <p>
                     { props.education.degree }
                 </p>
-                <hr/>
             </div>
-            { props.education.description.map((item) => <p key={item}> {item} </p>)}
+            <div className={ css.cardText }>
+                {props.education.description.map((item) => <> <li key={item}> ☞ {item} </li></>)}
+                <a target='blank' href={ props.education.website } className={ css.button }> Visit Site <HiOutlineExternalLink /> </ a>
+            </div>
         </div>
     )
 }
