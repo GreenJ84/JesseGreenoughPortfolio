e7cf3afad8d56c7faa5742acf5aae1c8472e915f
Jesse Greenough <jesse.greenough84@gmail.com>
2022-12-02 01:16:10 -0800

    Completed EduBody component

--- components/ExperiencePage/Education/EduBody.tsx
+++ components/ExperiencePage/Education/EduBody.tsx
@@ -1,9 +1,23 @@
 import React from 'react'
+import EduImage from './EduImage'
+
+const css = require('./EduBody.module.css')
 
 const EduBody = () => {
-  return (
-    <div>EduBody</div>
-  )
+    return (
+        <div style={{ display: 'flex', padding: '2rem 3rem 0 1rem'}}>
+            <div>
+                <EduImage />
+            </div>
+            <div className={ css.bodyText }>
+                <h1> Education </h1>
+                <h3>
+                    Basic Qualifications and Certifications 
+                </h3>
+                <p> I activiely participate in tech-related activities and partake in courses to further my understanding and knowledge. </p>
+            </div>
+        </div>
+    )
 }
 
 export default EduBody
