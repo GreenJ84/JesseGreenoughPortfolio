6be658e3934e246209aacd5e65878b5457485758
Jesse Greenough <jesse.greenough84@gmail.com>
2022-12-02 02:18:55 -0800

    Created impleent card shell with minimal styles

--- components/ExperiencePage/Education/Degree.tsx
+++ components/ExperiencePage/Education/Degree.tsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
+import DegreeCard from './DegreeCard'
+import { education } from '../../../Utils/data/EducationData'
+import Image from 'next/image'
+
+const css = require('./Degree.module.css')
 
 const Degree = () => {
-  return (
-    <div>Degree</div>
-  )
+    const [educationExp, setEducationExp] = useState( education );
+    
+
+
+    return (
+        <>
+            <h1 className={ css.title }> Degrees Recieved </h1>
+            { educationExp.map((exp) => <div key={ exp.college } className={ css.icons }>
+                <Image src={exp.icon} width={ 200 } height={ 200 } alt={`${exp.college} icon`} className={ css.image } /> 
+                <DegreeCard education={ exp } />
+            </div>)}
+        </>
+    )
 }
 
 export default Degree
