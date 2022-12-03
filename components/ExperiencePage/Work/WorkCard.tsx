714daa6c46b38dc7db1a3de8288e8abf057b8137
Jesse Greenough <jesse.greenough84@gmail.com>
2022-12-03 00:34:17 -0800

    Updated component with all logic

--- components/ExperiencePage/Work/WorkCard.tsx
+++ components/ExperiencePage/Work/WorkCard.tsx
@@ -1,14 +1,39 @@
 import React from "react"
+import Image from 'next/image'
 import { workItem } from "../../../Utils/data/WorkData"
+import { ImPointRight } from "react-icons/im"
+
+const css = require('./WorkCard.module.css')
 
 interface workCard{
     work: workItem
 }
 
-const WorkCard = ( prop: workCard ) => {
+const WorkCard = ( props: workCard ) => {
     return (
-        <div>
-
+        <div className={ css.workCard }>
+            <div className={ css.workImageHolder }>
+                <Image src={props.work.logo} width={20} height={20} className={css.workImage} alt={ props.work.company } />
+            </div>
+            <div className={ css.workCardBody }>
+                <div>
+                    <p>
+                        {props.work.position + ' '}
+                    </p>
+                    <p>
+                        {props.work.date}
+                    </p>
+                </div>
+                <div>
+                    <p>
+                        {props.work.company}
+                    </p>
+                    <p>
+                        {props.work.location}
+                    </p>
+                </div>
+                { props.work.details.map(( item, idx ) => <p key={idx}className={ css.details }> <ImPointRight /> {item} </p>)}
+            </div>
         </div>
     )
 }
