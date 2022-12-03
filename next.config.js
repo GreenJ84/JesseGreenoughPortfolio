205dfc90b0dc8a6d90bfa2a9965b69492ded18e0
Jesse Greenough <jesse.greenough84@gmail.com>
2022-12-03 00:34:34 -0800

    Updated config image permissions

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: ['github.com', 'imgs.search.brave.com'],
+    domains: ['github.com', 'imgs.search.brave.com', 'media.glassdoor.com'],
   },
 }
 
