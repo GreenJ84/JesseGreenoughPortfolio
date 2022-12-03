c71e0bfabe17a55534bc73c50142c1ddae6cd8fd
Jesse Greenough <jesse.greenough84@gmail.com>
2022-12-02 22:42:48 -0800

    Updated component and layed out style names

--- components/ExperiencePage/Work/WorkBody.tsx
+++ components/ExperiencePage/Work/WorkBody.tsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import WorkImg from './WorkImg'
+
+const css = require('./WorkBody.module.css')
 
 const WorkBody = () => {
-  return (
-    <div>WorkBody</div>
-  )
+    return (
+        <>
+            <h1 className={ css.workBodyTitle } >
+                Professional experience
+            </h1>
+            <div className={ css.workBody }>
+                <WorkImg />
+                <p>
+                    I have over 5 year of customer success and operations management experience. Areas of team relations, inventories, operations and more in a variety of environents from a face-paced college dining destination to a top department at my local groccery store. 
+                </p>
+            </div>
+        </>
+    )
 }
 
 export default WorkBody
