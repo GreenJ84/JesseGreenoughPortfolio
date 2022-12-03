99c8ef65f1e95c09511cb182cdc013b831c8702b
Jesse Greenough <jesse.greenough84@gmail.com>
2022-12-03 00:35:35 -0800

    Updated coponent with classNames and logic

--- components/ExperiencePage/Work/WorkExp.tsx
+++ components/ExperiencePage/Work/WorkExp.tsx
@@ -3,14 +3,38 @@ import { workHistory } from "../../../Utils/data/WorkData"
 import WorkBody from './WorkBody';
 import WorkCard from './WorkCard';
 
+const css = require('./WorkExp.module.css')
+
 const WorkExp = () => {
-    const [workData, setWorkData] = useState(workHistory);
+    const [workData, setWorkData] = useState( workHistory );
+    const [extraData, setExtraData] = useState( workHistory )
+    const [showWork, setShowWork] = useState(true);
+
+    const filterHandler = () => {
+        if (showWork) {
+            setShowWork(false)
+        } else {
+            setShowWork(true)
+        }
+    }
+
 
     return (
         <>
             <WorkBody />
-            <div>
-                {workData.map((item) => <WorkCard key={item.position} work={item} />)}
+            <div className={ css.workFilter }>
+                <p className={showWork ? css.activeFilter : ''} onClick={() => { if (!showWork){ filterHandler() }}}>
+                    Work
+                </p>
+                <p className={showWork ? '' : css.activeFilter} onClick={() => { if (showWork){ filterHandler() }}}>
+                    Internship/ Volunteer
+                </p>
+            </div>
+            <div className={ css.workCardHolder }>
+                {showWork ?
+                    workData.map((item) => <WorkCard key={item.position} work={item} />) :
+
+                    extraData.map((item) => <WorkCard key={item.position} work={item} />)}
             </div>
         </>
     )
