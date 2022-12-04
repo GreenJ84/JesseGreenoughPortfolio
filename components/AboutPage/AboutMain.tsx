dd0dd02ce6ce4c8f0625741e93f5cd5435b93cd7
Jesse Greenough <jesse.greenough84@gmail.com>
2022-12-04 13:08:25 -0800

    Responsiveness updates for AboutPage

--- components/AboutPage/AboutMain.tsx
+++ components/AboutPage/AboutMain.tsx
@@ -10,12 +10,11 @@ const css = require('./AboutMain.module.css')
 const AboutMain = () => {
     return (
         <Container className={ css.body }>
-            <h1 style={{ fontSize: "3em", paddingBottom: "20px", textAlign:'center' }}>
+            <h1 className={ css.title }>
                 Know Who <strong className="purple">I Am</strong>
             </h1>
             <AboutCard />
             <Image src={ laptopImage } alt="about" className={ css.image } />
-
         </Container>
     )
 }
