09f945a9d939d817ddb350fb01d1ff85b3e8f57d
Jesse Greenough <jesse.greenough84@gmail.com>
2022-12-13 15:14:45 -0800

    Degrees mobile responsiveness styling

--- components/ExperiencePage/Education/Degree.tsx
+++ components/ExperiencePage/Education/Degree.tsx
@@ -15,7 +15,7 @@ const Degree = ( props: Education) => {
     return (
         <>
             <h1 className={ css.title }> Degrees Recieved </h1>
-            {props.educationData.map((exp) => <div key={exp.college} className={css.icons}>
+            {props.educationData.map((exp) => <div key={exp.college} className={css.degrees}>
                 <a href={ exp.website }>
                     <Image src={exp.icon} width={200} height={200} alt={`${exp.college} icon`} className={css.image} />
                 </a>
