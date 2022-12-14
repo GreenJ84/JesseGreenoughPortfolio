29a33a3b7b97671482ea67851b4ab9ff04945056
Jesse Greenough <jesse.greenough84@gmail.com>
2022-12-13 18:25:38 -0800

    Typo update

--- components/ExperiencePage/Work/WorkBody.tsx
+++ components/ExperiencePage/Work/WorkBody.tsx
@@ -12,7 +12,7 @@ const WorkBody = () => {
             <div className={ css.workBody }>
                 <WorkImg />
                 <p>
-                    I have over 5 year of customer success and operations management experience. Areas of team relations, inventories, operations and more in a variety of environents from a face-paced college dining destination to a top department at my local groccery store. 
+                    I have over 5 year of customer success and operations management experience. Areas of team relations, inventories, operations and more in a variety of environents from a fast-paced college dining destination to a top department at my local groccery store. 
                 </p>
             </div>
         </>
