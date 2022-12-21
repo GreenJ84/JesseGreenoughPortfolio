27e961cf374ad0f7e1ca399269cc7610da03c58d
Jesse Greenough <jesse.greenough84@gmail.com>
2022-12-20 18:13:25 -0800

    Update classNames in component

--- components/HomePage/GithubCard.tsx
+++ components/HomePage/GithubCard.tsx
@@ -4,9 +4,8 @@ import GitHubCalendar from 'react-github-calendar'
 const css = require('./TechnicalSkills.module.css')
 
 const calenderStyle = {
-    
     margin: '1rem auto 4rem',
-    color: 'white',
+    color: 'var(--text-ternary)',
     height: '150px',
 }
 
@@ -15,14 +14,14 @@ const githubCard = () => {
     return (
         <>
             <h1 className={ css.projectHeading } >
-                Days I <strong className="purple">Code</strong>
+                Days I <strong className="detail">Code</strong>
             </h1>
             <GitHubCalendar
             username="GreenJ84"
             color="#0be116"
-                style={calenderStyle}
-                blockSize={14}
-                blockMargin={6}
+            style={calenderStyle}
+            blockSize={14}
+            blockMargin={6}
             />
         </>
     )
