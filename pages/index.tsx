d1903fff7a36957a8fb72036e43807b92750ba38
Jesse Greenough <jesse.greenough84@gmail.com>
2022-12-20 19:09:45 -0800

    Updated home page padding

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,7 @@ import HomeTop from "../components/HomePage/HomeTop";
 
 const HomePage = () => {
   return (
-    <section style={{ padding: '0 2vw'}}>
+    <section style={{ padding: '10rem 2vw 0'}}>
       <HomeTop />
       <HomeBottom />
     </section>
