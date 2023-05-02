3ce3147de6fcc0c2a1e636a60ffc14c9c4d77ebf
Jesse Greenough <jesse.greenough84@gmail.com>
2023-05-02 14:32:53 -0700

    Update and format cypress config

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,4 +1,6 @@
-import { defineConfig } from 'cypress'
+/** @format */
+
+import { defineConfig } from "cypress";
 
 export default defineConfig({
   defaultCommandTimeout: 10000,
@@ -7,7 +9,22 @@ export default defineConfig({
   pageLoadTimeout: 10000,
   video: true,
   videoUploadOnPasses: false,
-  videosFolder: 'cypress/videos',
-  screenshotsFolder: 'cypress/screenshots',
-  env: {},
-})
+  videosFolder: "cypress/videos",
+  screenshotsFolder: "cypress/screenshots",
+  fixturesFolder: "cypress/fixtures",
+  chromeWebSecurity: false,
+  env: {
+    NODE_TLS_REJECT_UNAUTHORIZED: "0"
+  },
+  numTestsKeptInMemory: 5,
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+    baseUrl: "http://localhost:3000/",
+    specPattern: "cypress/e2e/**/*.cy.{ts,tsx}",
+    excludeSpecPattern: "cypress/e2e/examples/*.cy.{ts,tsx}",
+    slowTestThreshold: 10000,
+    testIsolation: true,
+  },
+});
