eda20effa48c5837956ac811060ceed127d5ab2a
Jesse Greenough <jesse.greenough84@gmail.com>
2023-05-04 14:33:32 -0700

    Clean, format, and update component semantics

--- components/HomePage/Databases.tsx
+++ components/HomePage/Databases.tsx
@@ -11,17 +11,17 @@ const css = require("./TechnicalSkills.module.css");
 const Databases = () => {
   return (
     <div id="development-databases">
-      <h1 className={css.projectHeading}>
-        <strong className="detail">Databases </strong>
-      </h1>
-      <div id="databases" className={css.body}>
+      <h3 className={css.skillSectionTitle}>
+        <b className="detail">Databases </b>
+      </h3>
+      <ul id="databases" className={css.skillsListContainer}>
         <TechnicalSkill name="MongoDB" rating={5} icon={<SiMongodb />} />
         <TechnicalSkill name="MySQL" rating={4} icon={<GrMysql />} />
         <TechnicalSkill name="SQLite" rating={3} icon={<DiSqllite />} />
         <TechnicalSkill name="Redis" rating={2} icon={<DiRedis />} />
         <TechnicalSkill name="PostgresQL" rating={3} icon={<SiPostgresql />} />
         <TechnicalSkill name="DynamoDB" rating={1} icon={<SiAmazondynamodb />} />
-      </div>
+      </ul>
     </div>
   );
 };
