a43a118e9c367280e680c70aad6d1a2524bd8bc1
Jesse Greenough <jesse.greenough84@gmail.com>
2023-05-03 23:58:40 -0700

    Add Rust language and update language skill rating

--- components/HomePage/Languages.tsx
+++ components/HomePage/Languages.tsx
@@ -4,7 +4,7 @@ import React from "react";
 import { Col } from "react-bootstrap";
 
 import { DiCss3, DiJavascript1, DiPython } from "react-icons/di";
-import { FaSwift } from "react-icons/fa";
+import { FaSwift, FaRust } from "react-icons/fa";
 import {
   SiScratch,
   SiGoland,
@@ -21,7 +21,7 @@ const css = require("./TechnicalSkills.module.css");
 
 const Languages = () => {
   return (
-    <>
+    <div id="development-languages">
       <h1 className={css.projectHeading}>
         Programming <strong className="detail">Languages </strong>
       </h1>
@@ -32,16 +32,17 @@ const Languages = () => {
         <TechnicalSkill name="JavaScript" rating={5} icon={<DiJavascript1 />} />
         <TechnicalSkill name="TypeScript" rating={5} icon={<SiTypescript />} />
         <TechnicalSkill name="Python" rating={4} icon={<DiPython />} />
-        <TechnicalSkill name="SQL" rating={4} icon={<AiOutlineConsoleSql />} />
         <TechnicalSkill name="GraphQL" rating={4} icon={<SiGraphql />} />
+        <TechnicalSkill name="SQL" rating={4} icon={<AiOutlineConsoleSql />} />
         <TechnicalSkill name="Scratch" rating={4} icon={<SiScratch />} />
-        <TechnicalSkill name="Solidity" rating={3} icon={<SiSolidity />} />
-        <TechnicalSkill name="Java" rating={3} icon={<SiJava />} />
         <TechnicalSkill name="Markdown" rating={3} icon={<BsFillMarkdownFill />} />
+        <TechnicalSkill name="Rust" rating={3} icon={<FaRust />} />
+        <TechnicalSkill name="Java" rating={3} icon={<SiJava />} />
+        <TechnicalSkill name="Solidity" rating={2} icon={<SiSolidity />} />
         <TechnicalSkill name="GO" rating={1} icon={<SiGoland />} />
         <TechnicalSkill name="Swift" rating={1} icon={<FaSwift />} />
       </div>
-    </>
+    </div>
   );
 };
 
