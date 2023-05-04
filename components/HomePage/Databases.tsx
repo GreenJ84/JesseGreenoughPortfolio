40019da07f43fab175e8227b79d0ed28444256aa
Jesse Greenough <jesse.greenough84@gmail.com>
2023-05-03 23:57:58 -0700

    Update database developed with

--- components/HomePage/Databases.tsx
+++ components/HomePage/Databases.tsx
@@ -1,8 +1,7 @@
 /** @format */
 
 import React from "react";
-import { Col } from "react-bootstrap";
-import { DiSqllite } from "react-icons/di";
+import { DiSqllite, DiRedis } from "react-icons/di";
 import { GrMysql } from "react-icons/gr";
 import { SiAmazondynamodb, SiMongodb, SiPostgresql } from "react-icons/si";
 import TechnicalSkill from "./TechnicalSkill";
@@ -11,18 +10,19 @@ const css = require("./TechnicalSkills.module.css");
 
 const Databases = () => {
   return (
-    <>
+    <div id="development-databases">
       <h1 className={css.projectHeading}>
         <strong className="detail">Databases </strong>
       </h1>
       <div className={css.body}>
         <TechnicalSkill name="MongoDB" rating={5} icon={<SiMongodb />} />
         <TechnicalSkill name="MySQL" rating={4} icon={<GrMysql />} />
+        <TechnicalSkill name="SQLite" rating={3} icon={<DiSqllite />} />
+        <TechnicalSkill name="Redis" rating={2} icon={<DiRedis />} />
         <TechnicalSkill name="PostgresQL" rating={3} icon={<SiPostgresql />} />
         <TechnicalSkill name="DynamoDB" rating={1} icon={<SiAmazondynamodb />} />
-        <TechnicalSkill name="SQLite" rating={1} icon={<DiSqllite />} />
       </div>
-    </>
+    </div>
   );
 };
 
