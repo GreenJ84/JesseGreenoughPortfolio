d3622edaf8fd635b7eb3ad703bb94e9dfa329ba2
Jesse Greenough <jesse.greenough84@gmail.com>
2023-05-03 23:59:57 -0700

    Create a page object for the home page

---
+++ cypress/page-objects/MainPage.ts
@@ -0,0 +1,9 @@
+export class MainPage{
+    homeTop = cy.get(".homeSection");
+    homeDesctription = cy.get(".homeAboutDescription");
+    languages = cy.get("#development-languages");
+    framework = cy.get("#development-frameworks");
+    databases = cy.get("#development-databases");
+    developerTools = cy.get("#developer-tools");
+    socialSection = cy.get(".homeAboutSocial");
+}
