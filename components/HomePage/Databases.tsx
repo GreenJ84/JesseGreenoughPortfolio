4e00a15595809f61c263ec10893306a090106942
Jesse Greenough <jesse.greenough84@gmail.com>
2023-05-05 23:17:16 -0700

    Update icon classes

--- components/HomePage/Databases.tsx
+++ components/HomePage/Databases.tsx
@@ -14,13 +14,40 @@ const Databases = () => {
       <h3 className={css.skillSectionTitle}>
         <b className="detail">Databases </b>
       </h3>
-      <ul id="databases" className={css.skillsListContainer}>
-        <TechnicalSkill name="MongoDB" rating={5} icon={<SiMongodb />} />
-        <TechnicalSkill name="MySQL" rating={4} icon={<GrMysql />} />
-        <TechnicalSkill name="SQLite" rating={3} icon={<DiSqllite />} />
-        <TechnicalSkill name="Redis" rating={2} icon={<DiRedis />} />
-        <TechnicalSkill name="PostgresQL" rating={3} icon={<SiPostgresql />} />
-        <TechnicalSkill name="DynamoDB" rating={1} icon={<SiAmazondynamodb />} />
+      <ul
+        id="databases"
+        className={css.skillsListContainer}
+      >
+        <TechnicalSkill
+          name="MongoDB"
+          rating={5}
+          icon={<SiMongodb className={css.icon} />}
+        />
+        <TechnicalSkill
+          name="MySQL"
+          rating={4}
+          icon={<GrMysql className={css.icon} />}
+        />
+        <TechnicalSkill
+          name="SQLite"
+          rating={3}
+          icon={<DiSqllite className={css.icon}/>}
+        />
+        <TechnicalSkill
+          name="Redis"
+          rating={3}
+          icon={<DiRedis className={css.icon}/>}
+        />
+        <TechnicalSkill
+          name="PostgresQL"
+          rating={2}
+          icon={<SiPostgresql className={css.icon}/>}
+        />
+        <TechnicalSkill
+          name="DynamoDB"
+          rating={1}
+          icon={<SiAmazondynamodb className={css.icon}/>}
+        />
       </ul>
     </div>
   );
