e3ed13927719e26471968aae5293069df94de881
Jesse Greenough <jesse.greenough84@gmail.com>
2023-05-06 10:43:09 -0700

    Clean, format, and update attributes & semantics

--- components/AboutPage/AboutCard.tsx
+++ components/AboutPage/AboutCard.tsx
@@ -1,7 +1,6 @@
 /** @format */
 
 import React from "react";
-import { Card } from "react-bootstrap";
 
 import { ImPointRight } from "react-icons/im";
 
@@ -9,52 +8,54 @@ const css = require("./AboutCard.module.css");
 
 const AboutCard = () => {
   return (
-    <Card className={css.quoteCardView}>
-      <Card.Body>
-        <blockquote className={css.blockquote}>
-          <p style={{ textAlign: "left" }}>
-            Hi everyone, I am <span className="detail">Jesse Greenough </span>{" "}
-            currently residing in{" "}
-            <span className="detail"> Shoreline, Washington USA.</span>
-            <br />
-            <br />
-            I am a Full-Stack Developer with 5+ years of previous experience in team operations, business management,
-            and customer success.
-            <br />
-            <br />
-            Can you tell my favorite color is{" "}
-            <span className="detail">Green</span>??? ( All Shades 😎 )
-            <br />
-            <br />
-            Apart from learning and coding, some other activities that I love to
-            do are:
-          </p>
-          <ul>
-            <li className={css.aboutActivity}>
-              <ImPointRight /> Spend time with my amazing family
-            </li>
-            <li className={css.aboutActivity}>
-              <ImPointRight /> Enjoy the outdoors!
-            </li>
-            <li className={css.aboutActivity}>
-              <ImPointRight /> NFT Art collecting (Currently have Eth, HBAR, and XRP NFTs)
-            </li>
-            <li className={css.aboutActivity}>
-              <ImPointRight /> Web3 community participation
-            </li>
-            <li className={css.aboutActivity}>
-              <ImPointRight /> Play Sports (Basketball!)
-            </li>
-            <li className={css.aboutActivity}>
-              <ImPointRight /> Play video games/Watch anime 
-            </li>
-            <li className={css.aboutActivity}>
-              <ImPointRight /> Do Sudoku or word puzzles
-            </li>
-          </ul>
-        </blockquote>
-      </Card.Body>
-    </Card>
+    <div className={css.aboutOpener}>
+      <p style={{ textAlign: "left" }}>
+        Hi everyone, I am<span className="detail"> Jesse Greenough </span>
+        currently residing in
+        <span className="detail"> Shoreline, Washington USA.</span>
+        <br />
+        <br />
+        I am a Full-Stack Developer with 5+ years of previous experience in team
+        operations, business management, and customer success.
+        <br />
+        <br />
+        Can you tell my favorite color is
+        <span className="detail"> Green</span>??? ( All Shades 😎 )
+        <br />
+        <br />
+        Apart from learning and coding, some other activities that I love to do
+        are:
+      </p>
+      <ul>
+        <li>
+          <ImPointRight className={css.pointer} /> &emsp;Spend time with my
+          amazing family
+        </li>
+        <li>
+          <ImPointRight className={css.pointer} /> &emsp;Enjoy the outdoors!
+        </li>
+        <li>
+          <ImPointRight className={css.pointer} /> &emsp;NFT Art collecting
+          (Currently have Eth, HBAR, and XRP NFTs)
+        </li>
+        <li>
+          <ImPointRight className={css.pointer} /> &emsp;Web3 community
+          participation
+        </li>
+        <li>
+          <ImPointRight className={css.pointer} /> &emsp;Play Sports
+          (Basketball!)
+        </li>
+        <li>
+          <ImPointRight className={css.pointer} /> &emsp;Play video games/Watch
+          anime
+        </li>
+        <li>
+          <ImPointRight className={css.pointer} /> &emsp;Do Sudoku or word
+          puzzles
+        </li>
+      </ul>
+    </div>
   );
 };
 
