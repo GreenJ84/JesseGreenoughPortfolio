64a193422dce721df0debf0fd884a2e90e4fa284
Jesse Greenough <jesse.greenough84@gmail.com>
2023-05-06 22:06:21 -0700

    Update main page object construction and selectors

--- cypress/page-objects/MainPage.ts
+++ cypress/page-objects/MainPage.ts
@@ -1,10 +1,23 @@
 /// <reference types="cypress" />
+
+import { BASEURL } from "../support/e2e";
 export class MainPage{
-    homeTop = cy.get("#homeIntro");
-    homeDesctription = cy.get("#developerIntro");
-    languages = cy.get("#development-languages");
-    framework = cy.get("#development-frameworks");
-    databases = cy.get("#development-databases");
-    developerTools = cy.get("#developer-tools");
-    socialSection = cy.get("#developerSocial");
+    homeTop: Cypress.Chainable<JQuery<HTMLElement>>;
+    homeDesctription: Cypress.Chainable<JQuery<HTMLElement>>;
+    languages: Cypress.Chainable<JQuery<HTMLElement>>;
+    framework: Cypress.Chainable<JQuery<HTMLElement>>;
+    databases: Cypress.Chainable<JQuery<HTMLElement>>;
+    developerTools: Cypress.Chainable<JQuery<HTMLElement>>;
+    socialSection: Cypress.Chainable<JQuery<HTMLElement>>;
+
+    constructor() {
+        cy.visit(BASEURL);
+        this.homeTop = cy.get("#homeIntro");
+        this.homeDesctription = cy.get("#developerIntro");
+        this.languages = cy.get("#development-languages");
+        this.framework = cy.get("#development-frameworks");
+        this.databases = cy.get("#development-databases");
+        this.developerTools = cy.get("#developer-tools");
+        this.socialSection = cy.get("#developerSocial");
+    }
 }
