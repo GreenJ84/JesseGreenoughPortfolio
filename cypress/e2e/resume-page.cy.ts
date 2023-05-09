524a7a2368043b1db3e900a583764fb0e59a231c
Jesse Greenough <jesse.greenough84@gmail.com>
2023-05-09 03:17:16 -0700

    Detail out test case descriptions

--- cypress/e2e/resume-page.cy.ts
+++ cypress/e2e/resume-page.cy.ts
@@ -2,10 +2,6 @@
 
 /// <reference types="cypress" />
 
-/** @format */
-
-/// <reference types="cypress" />
-
 import { ResumePage } from "../page-objects/ResumePage";
 import { BASEURL, viewports, setUpStandard } from "../support/e2e";
 
@@ -22,14 +18,58 @@ describe("Resume Page render testing at all viewport sizes", () => {
   });
 
   viewports.forEach((viewport) => {
-    describe(`Resume Page Main resume section is rendering as expected at size: ${
+    describe(`Resume Page container renders as expected at size: ${
       typeof viewport == "string"
         ? viewport
         : `${viewport.width} x ${viewport.height}`
     }`, () => {
       before(() => setUpStandard(viewport));
 
-      it("", () => {});
+      it("The resume Page renders the correct main layout", () => { });
+      
+      it("The resume Page renders the correct css styles", () => {});
+    });
+
+    describe(`Resume holder renders as expected at size: ${
+      typeof viewport == "string"
+        ? viewport
+        : `${viewport.width} x ${viewport.height}`
+    }`, () => {
+      before(() => setUpStandard(viewport));
+
+      it("The layout of the resume section is correct", () => { });
+      
+      it("The css styles of the resume section are correct", () => { });
+      
+      it("The resume image renders the correct contents", () => { });
+    });
+
+    describe(`Resume navigation renders as expected at size: ${
+      typeof viewport == "string"
+        ? viewport
+        : `${viewport.width} x ${viewport.height}`
+    }`, () => {
+      before(() => setUpStandard(viewport));
+
+      it("The resume navigation correctly cycles through resumes and renders based on viewport", () => {
+        expect(true).to.be.true;
+      });
+    });
+
+    describe(`Resume Page buttons are rendering as expected at size: ${
+      typeof viewport == "string"
+        ? viewport
+        : `${viewport.width} x ${viewport.height}`
+    }`, () => {
+      before(() => setUpStandard(viewport));
+
+      it("The correct amount of button groups render", () => { 
+        resumePage.resumeHolder
+      });
+      
+      it("The buttons inside each group have the correct layout", () => { });
+      
+      it("The buttons in each group have the correct css styling", () => { });
     });
   });
 });
