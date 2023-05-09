b4b9ef0559002f9498b7794a65cf1355080677e6
Jesse Greenough <jesse.greenough84@gmail.com>
2023-05-09 01:30:05 -0700

    add Paragraph text testing method and snippets

--- cypress/page-objects/AboutPage.ts
+++ cypress/page-objects/AboutPage.ts
@@ -1,5 +1,13 @@
 import { BASEURL } from "../support/e2e";
 
+const PARAGRAPH_SNIPPETS = [
+    ["I was born and raise in the border city of El Paso, Texas.", "which is where my fascination with nature (GREEN-ery =&gt; GREEN-ough 🤣)", "big parts of my identity to this day."],
+    ["attended a Running Start focused high school", "receive my Associates degree my junior year of high school", "introduced to coding for the first time throught the robotics club"],
+    ["College ended up being one of the hardest times of my life.", "In my second semester of college my father&apos;s mental health deteriorated", "a very unstable environment for me mentally"],
+    ["only forseeable option to start earning towards paying off accumulated debt.", "from starting positions to lead positions", "blessed with a beautiful daughter"],
+    ["kept learning bits and pieces of code here and there", "a Full-stack Software Developer across multiple stacks", "continuously been increasing my knowledge"]
+];
+
 export class AboutPage {
     aboutMain: Cypress.Chainable<JQuery<HTMLElement>>;
     aboutIntro: Cypress.Chainable<JQuery<HTMLElement>>;
@@ -26,6 +34,24 @@ export class AboutPage {
             .should("have.length", 1)
             .next()
             .contains("")
+    }
+
+    testDetailParagraphsStyle(para: Cypress.Chainable<JQuery<HTMLParagraphElement>>) { 
+        para
+            .should("have.css", "position", "relative")
+            .should("have.css", "margin", "2rem auto")
+            .should("have.css", "text-indent", "3rem")
+            .should("have.css", "color", "rgb(206, 255, 208)")
+    }
+
+    testDetailParagraphText(para: Cypress.Chainable<JQuery<HTMLParagraphElement>>) { 
+        for (let i = 0; i < 5; i++) { 
+            for (let desc of PARAGRAPH_SNIPPETS[i]) {
+                para
+                    .eq(i)
+                    .contains(desc)
+            }
+        }
     }
 
 }
