3da123ec1e81c56318ced68ffbc0b25a6bd310ba
Jesse Greenough <jesse.greenough84@gmail.com>
2023-05-08 19:08:15 -0700

    Rename Home page object and update properties

--- cypress/page-objects/HomePage.ts
+++ cypress/page-objects/HomePage.ts
@@ -1,9 +1,9 @@
 /// <reference types="cypress" />
 
 import { BASEURL } from "../support/e2e";
-export class MainPage{
+export class HomePage {
     homeTop: Cypress.Chainable<JQuery<HTMLElement>>;
-    homeDesctription: Cypress.Chainable<JQuery<HTMLElement>>;
+    developerInto: Cypress.Chainable<JQuery<HTMLElement>>;
     languages: Cypress.Chainable<JQuery<HTMLElement>>;
     framework: Cypress.Chainable<JQuery<HTMLElement>>;
     databases: Cypress.Chainable<JQuery<HTMLElement>>;
@@ -13,7 +13,7 @@ export class MainPage{
     constructor() {
         cy.visit(BASEURL);
         this.homeTop = cy.get("#homeIntro");
-        this.homeDesctription = cy.get("#developerIntro");
+        this.developerInto = cy.get("#developerIntro");
         this.languages = cy.get("#development-languages");
         this.framework = cy.get("#development-frameworks");
         this.databases = cy.get("#development-databases");
