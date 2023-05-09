f90a8a1549951bd82ae2455b34a10bda11d08cb0
Jesse Greenough <jesse.greenough84@gmail.com>
2023-05-08 18:42:25 -0700

    Format, move skill container testing to page object and implement

--- cypress/e2e/home-page.cy.ts
+++ cypress/e2e/home-page.cy.ts
@@ -1,33 +1,30 @@
 /** @format */
 
-// / <reference types="cypress" />
+/// <reference types="cypress" />
 
-import { MainPage } from "../../page-objects/MainPage";
-import { BASEURL, VIEW, viewports } from "../../support/e2e";
+import { MainPage } from "../page-objects/MainPage";
+import { BASEURL, viewports, setUpStandard } from "../support/e2e";
 
 let mainPage: MainPage;
-const setUpStandard = (viewport: VIEW) => {
-  cy.log(`Viewport set to: ${typeof viewport == "string" ? viewport : `${viewport.width} x ${viewport.height}`}`);
-  if (typeof viewport === "string") {
-    cy.viewport(viewport as Cypress.ViewportPreset)
-      .then(() => cy.window().trigger('resize'));
-  } else {
-    cy.viewport(viewport.width, viewport.height)
-      .then(() => cy.window().trigger('resize'));
-  }
-  cy.visit(BASEURL);
-}
+const HOMEURL = `${BASEURL}/`;
 
-
-describe("Home page Compatibility at all viewports", () => {
+describe("Home Page render testing at all viewport sizes", () => {
   before(() => {
-    mainPage = new MainPage()
+    mainPage = new MainPage();
+    cy.window().then((win) => {
+      win.localStorage.setItem("theme", "dark");
+    });
   });
 
   viewports.forEach((viewport) => {
-    describe(`Home page Introduction visable and as expected at size: ${typeof viewport == "string" ? viewport : `${viewport.width} x ${viewport.height}`}`, () => {
+    describe(`Home page Introduction visable and rendering as expected at size: ${
+      typeof viewport == "string"
+        ? viewport
+        : `${viewport.width} x ${viewport.height}`
+    }`, () => {
       before(() => {
-        setUpStandard(viewport)
+        setUpStandard(viewport);
+        cy.visit(HOMEURL);
       });
 
       //! Move to navigation testing
@@ -64,7 +61,7 @@ describe("Home page Compatibility at all viewports", () => {
           .next()
           .children()
           .should("have.length", 3);
-    });
+      });
 
       it("The Home developer logo should be correct and visible", () => {
         mainPage.homeTop
@@ -75,7 +72,11 @@ describe("Home page Compatibility at all viewports", () => {
       });
     });
 
-    describe(`Technical Sections are rendering as expected at size: ${typeof viewport == "string" ? viewport : `${viewport.width} x ${viewport.height}`}`, () => {
+    describe(`Each Technical Section is rendering and as expected at size: ${
+      typeof viewport == "string"
+        ? viewport
+        : `${viewport.width} x ${viewport.height}`
+    }`, () => {
       before(() => {
         setUpStandard(viewport);
       });
@@ -119,145 +120,219 @@ describe("Home page Compatibility at all viewports", () => {
       it(`The Github card is being displayed`, () => {
         cy.wait(1000);
         cy.window().then((win) => {
-          cy.log(""+win.innerWidth);
+          cy.log("" + win.innerWidth);
           if (win.innerWidth > 1000) {
-            cy.get("#githubCard").debug()
+            cy.get("#githubCard")
+              .debug()
               .should("be.visible")
               .children()
               .should("have.length", 2)
               .first()
               .should("have.text", "Days I Code")
               .next()
-              .should("be.visible")
+              .should("be.visible");
           } else {
-            cy.get("#githubCard")
-              .should("not.exist")
+            cy.get("#githubCard").should("not.exist");
           }
         });
       });
 
       it("The Developer tools section titles and lists are rendering correctly", () => {
-        mainPage.developerTools
-          .within(() => {
-            cy.get("h1")
-              .should("have.text", "Development Tools");
+        mainPage.developerTools.within(() => {
+          cy.get("h1").should("have.text", "Development Tools");
 
-            cy.get("h5")
-              .eq(0)
-              .contains("Governance/Planning")
-              .next()
-              .children()
-              .should("have.length", 4);
+          cy.get("h5")
+            .eq(0)
+            .contains("Governance/Planning")
+            .next()
+            .children()
+            .should("have.length", 4);
 
-            cy.get("h5")
-              .eq(1)
-              .contains("Development")
-              .next()
-              .children()
-              .should("have.length", 12);
+          cy.get("h5")
+            .eq(1)
+            .contains("Development")
+            .next()
+            .children()
+            .should("have.length", 12);
 
-            cy.get("h5")
-              .eq(2)
-              .contains("Runtime")
-              .next()
-              .children()
-              .should("have.length", 5);
+          cy.get("h5")
+            .eq(2)
+            .contains("Runtime")
+            .next()
+            .children()
+            .should("have.length", 5);
 
-            cy.get("h5")
-              .eq(3)
-              .contains("DevOps/Serving")
-              .next()
-              .children()
-              .should("have.length", 9);
+          cy.get("h5")
+            .eq(3)
+            .contains("DevOps/Serving")
+            .next()
+            .children()
+            .should("have.length", 9);
 
-            cy.get("h5")
-              .eq(4)
-              .contains("Extras")
-              .next()
-              .children()
-              .should("have.length", 6);
-          });
+          cy.get("h5")
+            .eq(4)
+            .contains("Extras")
+            .next()
+            .children()
+            .should("have.length", 6);
+        });
       });
     });
 
-    describe(`Technical Skills blocks are rendering as expected  at size: ${typeof viewport == "string" ? viewport : `${viewport.width} x ${viewport.height}`}`, () => {
+    describe(`Technical Skills containers are rendering as expected within each section at size: ${
+      typeof viewport == "string"
+        ? viewport
+        : `${viewport.width} x ${viewport.height}`
+    }`, () => {
       before(() => setUpStandard(viewport));
 
+      it("The Language Technical skills containers renders correctly", () => {
+        mainPage.testSkillContainer(
+          mainPage.languages
+            .should("be.visible")
+            .find("#languages")
+            .children()
+            .eq(Math.floor(Math.random() * 14))
+        );
+      });
 
-      it("The Technical skills containers in the languages section works correctly", () => {
-        mainPage.languages
-          .should("be.visible")
-          .find("#languages")
-          .children()
-          .eq(Math.floor(Math.random() * 14))
-          .should("have.length", 3)
-          .and("be.visible");
-      })
+      it("The Language Technical skills container is interactive", () => {
+        mainPage.hoverSkillContainer(
+          mainPage.languages
+            .should("be.visible")
+            .find("#languages")
+            .children()
+            .eq(Math.floor(Math.random() * 14))
+        );
+      });
 
-      it("The Technical skills containers in the frameworks section works correctly", () => {
-        mainPage.framework
-          .should("be.visible")
-          .find("#frameworks")
-          .children()
-          .eq(Math.floor(Math.random() * 17))
-          .should("have.length", 3)
-          .and("be.visible");
-      })
+      it("The Framework Technical skills containers renders correctly", () => {
+        mainPage.testSkillContainer(
+          mainPage.framework
+            .should("be.visible")
+            .find("#frameworks")
+            .children()
+            .eq(Math.floor(Math.random() * 17))
+        );
+      });
 
-      it("The Technical skills containers in the databases section works correctly", () => {
-        mainPage.databases
-          .should("be.visible")
-          .find("#databases")
-          .children()
-          .eq(Math.floor(Math.random() * 6))
-          .should("have.length", 3)
-          .and("be.visible");
-      })
+      it("The Framework Technical skills containers is interactive", () => {
+        mainPage.hoverSkillContainer(
+          mainPage.framework
+            .should("be.visible")
+            .find("#frameworks")
+            .children()
+            .eq(Math.floor(Math.random() * 17))
+        );
+      });
 
-      it("The Technical skills containers in the developer tools section works correctly", () => {
-        mainPage.developerTools
-          .within(() => {
-            cy.get("ul")
-              .eq(0)
-              .children()
-              .eq(Math.floor(Math.random() * 4))
-              .should("have.length", 3)
-              .and("be.visible");
+      it("The Database Technical skills containers renders correctly", () => {
+        mainPage.testSkillContainer(
+          mainPage.databases
+            .should("be.visible")
+            .find("#databases")
+            .children()
+            .eq(Math.floor(Math.random() * 6))
+        );
+      });
 
-            cy.get("ul")
-              .eq(1)
-              .children()
-              .eq(Math.floor(Math.random() * 12))
-              .should("have.length", 3)
-              .and("be.visible");
+      it("The Database Technical skills containers is interactive", () => {
+        mainPage.hoverSkillContainer(
+          mainPage.databases
+            .should("be.visible")
+            .find("#databases")
+            .children()
+            .eq(Math.floor(Math.random() * 6))
+        );
+      });
 
-            cy.get("ul")
-              .eq(2)
-              .children()
-              .eq(Math.floor(Math.random() * 5))
-              .should("have.length", 3)
-              .and("be.visible");
+      it("The Developer Tool Technical skills containers renders correctly", () => {
+        mainPage.developerTools.within(() => {
+          let container: Cypress.Chainable<JQuery<HTMLElement>>;
+          container = cy
+            .get("ul")
+            .eq(0)
+            .children()
+            .eq(Math.floor(Math.random() * 4));
+          mainPage.testSkillContainer(container);
 
-            cy.get("ul")
-              .eq(3)
-              .children()
-              .eq(Math.floor(Math.random() * 9))
-              .should("have.length", 3)
-              .and("be.visible");
+          container = container = cy
+            .get("ul")
+            .eq(1)
+            .children()
+            .eq(Math.floor(Math.random() * 12));
+          mainPage.testSkillContainer(container);
 
-            cy.get("ul")
-              .eq(4)
-              .children()
-              .eq(Math.floor(Math.random() * 6))
-              .should("have.length", 3)
-              .and("be.visible");
-          });
-      })
-    })
+          container = cy
+            .get("ul")
+            .eq(2)
+            .children()
+            .eq(Math.floor(Math.random() * 5));
+          mainPage.testSkillContainer(container);
 
-    describe(`Developer Socials are rendering as expected and missing in the footer at size: ${typeof viewport == "string" ? viewport : `${viewport.width} x ${viewport.height}`}`, () => {
+          container = cy
+            .get("ul")
+            .eq(3)
+            .children()
+            .eq(Math.floor(Math.random() * 9));
+          mainPage.testSkillContainer(container);
+
+          container = cy
+            .get("ul")
+            .eq(4)
+            .children()
+            .eq(Math.floor(Math.random() * 6));
+          mainPage.testSkillContainer(container);
+        });
+      });
+
+      it("The Developer Tool Technical skills containers are interactive", () => {
+        mainPage.developerTools.within(() => {
+          let container: Cypress.Chainable<JQuery<HTMLElement>>;
+          container = cy
+            .get("ul")
+            .eq(0)
+            .children()
+            .eq(Math.floor(Math.random() * 4));
+          mainPage.hoverSkillContainer(container);
+
+          container = container = cy
+            .get("ul")
+            .eq(1)
+            .children()
+            .eq(Math.floor(Math.random() * 12));
+          mainPage.hoverSkillContainer(container);
+
+          container = cy
+            .get("ul")
+            .eq(2)
+            .children()
+            .eq(Math.floor(Math.random() * 5));
+          mainPage.hoverSkillContainer(container);
+
+          container = cy
+            .get("ul")
+            .eq(3)
+            .children()
+            .eq(Math.floor(Math.random() * 9));
+          mainPage.hoverSkillContainer(container);
+
+          container = cy
+            .get("ul")
+            .eq(4)
+            .children()
+            .eq(Math.floor(Math.random() * 6));
+          mainPage.hoverSkillContainer(container);
+        });
+      });
+    });
+
+    describe(`Developer Socials are rendering as expected and missing in the footer at size: ${
+      typeof viewport == "string"
+        ? viewport
+        : `${viewport.width} x ${viewport.height}`
+    }`, () => {
       before(() => setUpStandard(viewport));
-
 
       it("The Developer Socials section component and title are rendering correctly", () => {
         mainPage.developerSocials
@@ -266,46 +341,44 @@ describe("Home page Compatibility at all viewports", () => {
           .find("p")
           .should("have.length", 2)
           .and("have.text", "Feel free to connect with me!");
-      })
-
-      it("The Developer Socials section list, logos, and links are rendering", () => {
-        mainPage.developerSocials
-          .find("ul")
-          .within(() => {
-            cy.get("li")
-              .should("have.length", 4)
-              .eq(0)
-              .find("a")
-              .should("have.attr", "href")
-              .and("include", "https://www.linkedin.com")
-              .children()
-              .should("have.length", 1)
-        
-            cy.get("li")
-              .eq(1)
-              .find("a")
-              .should("have.attr", "href")
-              .and("include", "https://twitter.com")
-              .children()
-              .should("have.length", 1)
-        
-            cy.get("li")
-              .eq(3)
-              .find("a")
-              .should("have.attr", "href")
-              .and("include", "https://www.linkedin.com")
-              .children()
-              .should("have.length", 1)
-        
-            cy.get("li")
-              .eq(4)
-              .find("a")
-              .should("have.attr", "href")
-              .and("include", "https://www.instagram.com")
-              .children()
-              .should("have.length", 1)
-          })
       });
-    })
+
+      it("The Developer Socials section list, logos, and links are rendering correctly", () => {
+        mainPage.developerSocials.find("ul").within(() => {
+          cy.get("li")
+            .should("have.length", 4)
+            .eq(0)
+            .find("a")
+            .should("have.attr", "href")
+            .and("include", "https://www.linkedin.com")
+            .children()
+            .should("have.length", 1);
+
+          cy.get("li")
+            .eq(1)
+            .find("a")
+            .should("have.attr", "href")
+            .and("include", "https://twitter.com")
+            .children()
+            .should("have.length", 1);
+
+          cy.get("li")
+            .eq(3)
+            .find("a")
+            .should("have.attr", "href")
+            .and("include", "https://www.linkedin.com")
+            .children()
+            .should("have.length", 1);
+
+          cy.get("li")
+            .eq(4)
+            .find("a")
+            .should("have.attr", "href")
+            .and("include", "https://www.instagram.com")
+            .children()
+            .should("have.length", 1);
+        });
+      });
+    });
   });
 });
