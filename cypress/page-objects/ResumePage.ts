3c952e4fc05adfe2f25fbe30304fa093ab64600c
Jesse Greenough <jesse.greenough84@gmail.com>
2023-05-11 23:43:13 -0700

    Update syntax behind page object

--- cypress/page-objects/ResumePage.ts
+++ cypress/page-objects/ResumePage.ts
@@ -2,76 +2,84 @@
 /// <reference types="cypress" />
 
 export class ResumePage {
-  topButton: Cypress.Chainable<JQuery<HTMLElement>>;
-  resumeMain: Cypress.Chainable<JQuery<HTMLElement>>;
-  resume: Cypress.Chainable<JQuery<HTMLElement>>;
-  bottomButton: Cypress.Chainable<JQuery<HTMLElement>>;
-  constructor(url: string) {
-    cy.visit(url);
-    this.topButton = cy.get("#top-buttons");
-    this.resumeMain = cy.get("#resumePage");
-    this.resume = cy.get("#resume");
-    this.bottomButton = cy.get("#bottom-buttons");
+  topButton: Function;
+  resumeMain: Function;
+  resume: Function;
+  bottomButton: Function;
+  constructor() {
+    this.topButton = () => { return cy.get("#top-buttons")};
+    this.resumeMain = () => { return cy.get("#resumePage")};
+    this.resume = () => { return cy.get("#resume")};
+    this.bottomButton = () => { return cy.get("#bottom-buttons")};
   }
 
   testButtonGroupLayout(buttonGroup: Cypress.Chainable<JQuery<HTMLElement>>) {
-    buttonGroup
-      .children()
-      .should("have.length", 2)
-      .first()
-      .should("have.text", "Download")
-      .should('have.attr', 'target', '_blank')
-      .children()
-      .should("have.length", 2)
-
-    buttonGroup
-      .children()
-      .last()
-      .should("have.text", "View")
-      .should('have.attr', 'target', '_blank')
-      .children()
-      .should("have.length", 2)
+    buttonGroup.within(() => {
+      cy.get("a")
+        .children()
+        .should("have.length", 2);
+      
+      cy.get("a")
+        .first()
+        .should("include.text", "Download")
+        .and('have.attr', 'target', '_blank')
+        .and("have.attr", "role", "button")
+        .children()
+        .should("have.length", 1);
+  
+      cy.get("a")
+        .last()
+        .should("include.text", "View")
+        .and('have.attr', 'target', '_blank')
+        .and("have.attr", "role", "button")
+        .children()
+        .should("have.length", 1);
+    })
   };
 
   testButtonStyle(button: Cypress.Chainable<JQuery<HTMLButtonElement>>) {
     button
       .should('have.css', 'display', 'flex')
-      .should('have.css', 'align-items', 'center')
-      .should('have.css', 'justify-content', 'center')
-      .should('have.css', 'text-align', 'center')
-      .should('have.css', 'margin', 'center')
-      .should('have.css', 'color', 'rgb(0, 255, 13)')
-      .should('have.css', 'background-color', 'rgba(14, 215, 165, 0.924)')
-      .should('have.css', 'font-size', '1.8vw')
-      .should('have.css', 'font-weight', '900')
-      .should('have.css', 'border', '1px solid rgba(14, 215, 165, 0.924)')
-      .should('have.css', 'border-radius', '8px')
-      .should('have.css', 'box-shadow', '3px 3px 1px rgba(14, 215, 165, 0.924)')
-      .trigger('mouseover')
-      .should('have.css', 'color', 'rgb(36, 94, 36)')
-      .should('have.css', 'background-color', 'rgba(14, 215, 165, 0.924)')
-      .should('have.css', 'border-radius', '30px')
-      .should('have.css', 'font-weight', '800')
-      .should('have.css', 'box-shadow', '8px 8px 5px black')
-      .trigger('mouseout')
-      .should('have.css', 'color', 'rgb(0, 255, 13)')
-      .should('have.css', 'background-color', 'rgba(14, 215, 165, 0.924)')
-      .should('have.css', 'border-radius', '8px')
-      .should('have.css', 'font-weight', '900')
-      .should('have.css', 'box-shadow', '3px 3px 1px rgba(14, 215, 165, 0.924)')
+      .and('have.css', 'align-items', 'center')
+      .and('have.css', 'justify-content', 'center')
+      .and('have.css', 'text-align', 'center')
+      .and('have.css', 'color', 'rgb(0, 255, 13)')
+      .and('have.css', 'background-color', 'rgb(36, 94, 36)')
+      .and('have.css', 'font-weight', '900')
+      .and('have.css', 'border', '1px solid rgba(14, 215, 165, 0.925)')
+      .and('have.css', 'border-radius', '8px')
+      .then(($button) => {
+        expect($button).to.have.css('margin');
+        expect($button).to.have.css('box-shadow').to.match(/.*rgba\(14, 215, 165, 0\.925\).*/);
+        expect($button).to.have.css('font-size');
+      })
+  };
+
+  testButtonHover(button: Cypress.Chainable<JQuery<HTMLButtonElement>>) {
+    button.realHover()
+      .then(($button) => {
+        expect($button).to.have.css('color', 'rgb(36, 94, 36)')
+        expect($button).to.have.css('background-color', 'rgba(14, 215, 165, 0.925)')
+        expect($button).to.have.css('border-radius', '30px')
+        expect($button).to.have.css('font-weight', '800')
+        expect($button).to.have.css('box-shadow').to.match(/.*rgb\(0, 0, 0\).*/);
+    })
   };
 
   testVisibleNavArrows(navArrow: Cypress.Chainable<JQuery<HTMLElement>>) { 
+    // cy.log(`${navArrow}`)
     navArrow
       .should("be.visible")
-      .should("have.css", "position", "absolute")
-      .should("have.css", "display", "flex")
-      .should("have.css", "align-items", "center")
-      .should("have.css", "justify-content", "center")
-      .should("have.css", "padding", ".5vw")
-      .should("have.css", "color", "rgb(0, 255, 13)")
-      .should("have.css", "background-color", "rgba(210,210,210, .3)")
-      .should("have.css", "border-radius", "50%")
+      .and("have.css", "position", "absolute")
+      .and("have.css", "display", "flex")
+      .and("have.css", "align-items", "center")
+      .and("have.css", "justify-content", "center")
+      .and("have.css", "color", "rgb(0, 255, 13)")
+      .and("have.css", "background-color", "rgba(210, 210, 210, 0.3)")
+      .then(($div) => {
+        expect($div).to.have.css("padding");
+        expect($div).to.have.css("border-radius");
+      })
       .children()
       .should("have.length", 1);
   };
