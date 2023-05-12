ec75b816c8c38bb080d30f160fade678c2b00d07
Jesse Greenough <jesse.greenough84@gmail.com>
2023-05-11 23:46:46 -0700

    Update test syntax and naming

--- cypress/e2e/resume-page.cy.ts
+++ cypress/e2e/resume-page.cy.ts
@@ -3,7 +3,7 @@
 /// <reference types="cypress" />
 
 import { ResumePage } from "../page-objects/ResumePage";
-import { BASEURL, viewports, setUpStandard } from "../support/e2e";
+import { BASEURL, setupPageWithTheme, viewports, viewPortSetup } from "../support/e2e";
 
 let resumePage: ResumePage;
 const RESUMEURL = `${BASEURL}/resume`;
@@ -11,23 +11,36 @@ const RESUMEURL = `${BASEURL}/resume`;
 describe("Resume Page render testing at all viewport sizes", () => {
   before(() => {
     resumePage = new ResumePage();
-    cy.window().then((win) => {
-      win.localStorage.setItem("theme", "dark");
-    });
-    cy.visit(RESUMEURL);
   });
 
+  // let viewport = viewports[0];
   viewports.forEach((viewport) => {
     describe(`Resume Page container renders as expected at size: ${
       typeof viewport == "string"
         ? viewport
         : `${viewport.width} x ${viewport.height}`
     }`, () => {
-      before(() => setUpStandard(viewport));
+      before(() => viewPortSetup(viewport));
+      beforeEach(() => { setupPageWithTheme(RESUMEURL, "dark"); });
 
-      it("The resume Page renders the correct main layout", () => { });
+      it("The resume Page renders the correct main layout", () => { 
+        resumePage.resumeMain()
+          .should("be.visible")
+          .children()
+          .should("have.length", 3);
+
+        resumePage.resumeMain()
+          .within(() => {
+              resumePage.topButton().should("be.visible");
+              resumePage.resume().should("be.visible");
+              resumePage.bottomButton().should("be.visible");
+          });
+      });
       
-      it("The resume Page renders the correct css styles", () => {});
+      it("The resume Page renders the correct css styles", () => {
+        resumePage.resumeMain()
+          .should("have.css", "padding");
+      });
     });
 
     describe(`Resume holder renders as expected at size: ${
@@ -35,13 +48,49 @@ describe("Resume Page render testing at all viewport sizes", () => {
         ? viewport
         : `${viewport.width} x ${viewport.height}`
     }`, () => {
-      before(() => setUpStandard(viewport));
+      before(() => viewPortSetup(viewport));
+      beforeEach(() => { setupPageWithTheme(RESUMEURL, "dark"); });
 
-      it("The layout of the resume section is correct", () => { });
+      it("The layout of the resume section is correct", () => { 
+        resumePage.resume()
+          .should("be.visible")
+          .children()
+          .should("have.length", 3);
+
+        resumePage.resume()
+          .within(() => {
+            cy.get("img")
+              .should("have.length", 1);
+
+            cy.get("div")
+              .should("have.length", 2);
+          })
+      });
       
-      it("The css styles of the resume section are correct", () => { });
+      it("The css styles of the resume section are correct", () => {
+        resumePage.resume()
+          .should("have.css", "position", "relative")
+          .and("have.css", "display", "flex")
+          .and("have.css", "justify-content", "center")
+          .and("have.css", "margin");
+      });
       
-      it("The resume image renders the correct contents", () => { });
+
+      it("The resume navigation arrows render correctly with the viewport sizes", () => { 
+        let left = () => resumePage.resume()
+          .children("div")
+          .eq(0)
+        
+        let right = () => resumePage.resume()
+          .children("div")
+          .eq(1)
+          
+        // cy.log(`${left()} and ${right()}`);
+        resumePage.testVisibleNavArrows(left());
+        resumePage.testVisibleNavArrows(right());
+      });
+      
+      it("The resume image renders the correct contents and styles", () => { });
     });
 
     describe(`Resume navigation renders as expected at size: ${
@@ -49,27 +98,52 @@ describe("Resume Page render testing at all viewport sizes", () => {
         ? viewport
         : `${viewport.width} x ${viewport.height}`
     }`, () => {
-      before(() => setUpStandard(viewport));
+      before(() => viewPortSetup(viewport));
+      beforeEach(() => { setupPageWithTheme(RESUMEURL, "dark"); });
 
       it("The resume navigation correctly cycles through resumes and renders based on viewport", () => {
         expect(true).to.be.true;
       });
     });
 
+
+
+  
     describe(`Resume Page buttons are rendering as expected at size: ${
       typeof viewport == "string"
         ? viewport
         : `${viewport.width} x ${viewport.height}`
     }`, () => {
-      before(() => setUpStandard(viewport));
-
-      it("The correct amount of button groups render", () => { 
-        resumePage.resumeHolder
+      before(() => viewPortSetup(viewport));
+      beforeEach(() => { setupPageWithTheme(RESUMEURL, "dark"); });
+      
+      it("The correct amount of button groups render with the resume", () => { 
+        resumePage.resume()
+          .children("div")
+          .should("have.length", 2)
       });
       
-      it("The buttons inside each group have the correct layout", () => { });
+      it("The buttons inside each group have the correct layout", () => { 
+        resumePage.testButtonGroupLayout(resumePage.topButton());
+
+        resumePage.testButtonGroupLayout(resumePage.bottomButton());
+      });
       
-      it("The buttons in each group have the correct css styling", () => { });
+      it("The buttons in the top group have the correct css styling", () => { 
+        let currButton = resumePage.topButton().children("a").first();
+        resumePage.testButtonStyle(currButton);
+
+        currButton = resumePage.topButton().children("a").last();
+        resumePage.testButtonStyle(currButton);
+      });
+
+      it("The buttons in the bottom group have the correct css styling", () => {
+        let currButton = resumePage.bottomButton().children("a").first();
+        resumePage.testButtonStyle(currButton);
+
+        currButton = resumePage.bottomButton().children("a").last();
+        resumePage.testButtonStyle(currButton);
+      });
     });
   });
 });
