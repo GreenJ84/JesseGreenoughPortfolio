0adcf89bbcfcd57d2511963b5c263cb1b2a43294
Jesse Greenough <jesse.greenough84@gmail.com>
2023-05-12 22:20:03 -0700

    Seperate Brand component and imports

---
+++ components/Layout/NavBarBrand.tsx
@@ -0,0 +1,45 @@
+/** @format */
+
+import React from "react";
+import Navbar from "react-bootstrap/Navbar";
+import Image from "next/image";
+
+const logo1 = "/assets/logo.png";
+const logo2 = "/assets/CyberHedera1.png";
+
+const css = require("./NavBar.module.css");
+
+const NavBarBrand = (props: { mode: Function; theme: String }) => {
+
+  return (
+    <Navbar.Brand className={css.navBarBrand}>
+          { props.theme === "dark" ?
+              <p id="mode">🌞</p> :
+              <p id="mode">🌙</p>
+          }
+      <Image
+        src={props.theme === "dark" ? logo1 : logo2}
+        className={css.themeLogo}
+        alt="brand"
+        width={250}
+        height={250}
+        id="themeLogo"
+        onMouseEnter={() => {
+          document.getElementById("themeLogo")!.style.filter =
+            "grayscale(100%) contrast(200%) opacity(0.1)";
+          document.getElementById("mode")!.style.display = "block";
+        }}
+        onMouseLeave={() => {
+          document.getElementById("mode")!.style.display = "none";
+          document.getElementById("themeLogo")!.style.filter = "";
+        }}
+        onClick={(e: React.MouseEvent<HTMLImageElement>) => {
+          e.preventDefault();
+          props.mode();
+        }}
+      />
+    </Navbar.Brand>
+  );
+};
+
+export default NavBarBrand;
