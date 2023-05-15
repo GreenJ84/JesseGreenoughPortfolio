2748ebf795b2bdefb8bdc67addb933b38de8d93f
Jesse Greenough <jesse.greenough84@gmail.com>
2023-05-14 23:26:10 -0700

    Update cypress configurations

--- cypress.config.ts
+++ cypress.config.ts
@@ -7,19 +7,16 @@ export default defineConfig({
   requestTimeout: 50000,
   responseTimeout: 5000,
   pageLoadTimeout: 10000,
-  video: true,
+  video: false,
   videoUploadOnPasses: false,
   videosFolder: "cypress/videos",
   screenshotsFolder: "cypress/screenshots",
   fixturesFolder: "cypress/fixtures",
   chromeWebSecurity: false,
-
   env: {
     NODE_TLS_REJECT_UNAUTHORIZED: "0",
   },
-
   numTestsKeptInMemory: 5,
-
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
@@ -28,9 +25,8 @@ export default defineConfig({
     specPattern: "cypress/e2e/**/*.cy.{ts,tsx}",
     excludeSpecPattern: "cypress/e2e/examples/*.cy.{ts,tsx}",
     slowTestThreshold: 10000,
-    testIsolation: true,
+    testIsolation: false,
   },
-
   component: {
     devServer: {
       framework: "next",
