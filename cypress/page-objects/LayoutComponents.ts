8107c07e31b8b6d28e41c3ac2473a40b354e0561
Jesse Greenough <jesse.greenough84@gmail.com>
2023-05-17 23:57:55 -0700

    Update page test method selectors to correctly pass preset conditions

--- cypress/page-objects/LayoutComponents.ts
+++ cypress/page-objects/LayoutComponents.ts
@@ -33,11 +33,11 @@ export class LayoutComps {
         navItem
             .find("a")
             .should("have.length", 1)
-            .first()
+            .first().debug()
             .should("be.visible")
-            .should("contain.text", "")
-            .children()
-            .should("have.length", 1)
+            .should("contain.text", "").debug()
+            .find("svg")
+            .should("be.visible");
     }
     
     testNavItemStyle(navItem: Cypress.Chainable<JQuery<HTMLElement>>) {
@@ -56,20 +56,28 @@ export class LayoutComps {
         navItem.trigger("mouseout");
     }
 
-    testNavLinkStyle(navItem: Cypress.Chainable<JQuery<HTMLElement>>, width: Cypress.Chainable<number>) {
+    testNavLinkStyle(navItem: Cypress.Chainable<JQuery<HTMLElement>>, width: Cypress.Chainable<number>, idx: number) {
         navItem
             .find("a")
-            .first()
             .should("be.visible")
             .and("have.css", "display", "flex")
-            .and("have.css", "align-items", "center")
             .and("have.css", "font-weight", "400")
-            .and("have.css", "transition", "all 0.3s ease-out 0s")
-            .and("have.css", "position", "relative")
             .then(($link: JQuery<HTMLElement>) => {
-                expect($link).to.have.css("color").match(/rgb\(111, 255, 118\)/);
-                expect($link).to.have.css("text-shadow").match(/.*rgba\(0, 156, 13, 0\.87.*\)/);
-                expect($link).to.have.css("font-size");
+                if (idx !== 5) {
+                    expect($link).to.have.css("align-items");
+                    expect($link).to.have.css("transition");
+                    expect($link).to.have.css("color").match(/rgb\(111, 255, 118\)/);
+                    expect($link).to.have.css("text-shadow").match(/.*rgba\(0, 156, 13, 0\.87.*\)/);
+                    expect($link).to.have.css("font-size");
+                } else {
+                    expect($link).to.have.css("color").match(/rgb\(0, 33, 26\).*/);
+                    expect($link).to.have.css("background-color").match(/.*rgb\(36, 94, 36\).*/);
+                    expect($link).to.have.css("border-radius", "10px");
+                    expect($link).to.have.css("margin-left");
+                    expect($link).to.have.css("vertical-align");
+                    expect($link).to.have.css("text-align");
+                    expect($link).to.have.css("padding");
+                }
 
                 width.then((w) => {
                     if (w > 900) {
@@ -93,7 +101,7 @@ export class LayoutComps {
             .then((li) => {
                 expect(li).to.have.attr("href").contains(platforms[idx])
             })
-            .children()
+            .children("svg")
             .should("have.length", 1)
     }
 
@@ -102,16 +110,22 @@ export class LayoutComps {
             .then(($item: JQuery<HTMLElement>) => {
                 expect($item).to.have.css("padding");
             })
+        let link = footerLink
             .find("a")
             .first()
             .should("be.visible")
-            .and("have.css", "color", /.*rgb\(206, 255, 208\).*/)
             .then(($link: JQuery<HTMLElement>) => {
-                $link.trigger("mouseover");
-                expect($link).to.have.css("color").match(/.*rgb\(0, 255, 13\).*/);
-                $link.trigger("mouseout");
-            })
-            .children()
+                expect($link).to.have.css("color").match(/.*rgb\(206, 255, 208\).*/);
+            });
+        
+        link.trigger("mouseover");
+        link.trigger("mouseover");
+        link.then(($link) => {
+            expect($link).to.have.css("color");
+        });
+
+        link.trigger("mouseout");
+        link.children()
             .first()
             .should("be.visible")
             .and("have.css", "font-size");
