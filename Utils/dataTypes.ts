dda4b774afa8c64c3785923e7f3553e88700f9c3
Jesse Greenough <jesse.greenough84@gmail.com>
2023-05-28 00:21:49 -0700

    Migrate all data interfaces to single file

---
+++ Utils/dataTypes.ts
@@ -0,0 +1,44 @@
+/** @format */
+
+export interface workItem {
+  company: string;
+  logo: string;
+  position: string;
+  location: string;
+  date: string;
+  details: string[];
+}
+
+export interface IProject {
+  id: string;
+  priority?: number;
+  name: string;
+  date?: string;
+  description: string;
+  image_path: string;
+  deployed_url: string | null;
+  github_url: string;
+  category: string[];
+  key_techs: string[];
+}
+
+export interface educationType {
+  college: string;
+  degree: string;
+  date: string;
+  description: string[];
+  icon: string;
+  website: string;
+}
+
+export interface certificationType {
+  id?: string;
+  priority?: number;
+  title: string;
+  issuer: string;
+  date: string;
+  description: string;
+  image: string;
+  url: string;
+  tech?: string[];
+}
