5902ebbb1103b39911402503029032bcf2d7a4d6
Jesse Greenough <jesse.greenough84@gmail.com>
2023-05-29 01:03:52 -0700

    Create Experience page object for referencing key testing elements

--- cypress/page-objects/ExperiencePage.ts
+++ cypress/page-objects/ExperiencePage.ts
@@ -1,5 +1,78 @@
-export class ExperiencePage{
-    constructor(){
+/** @format */
 
+/// <reference types="cypress" />
+
+export class ExperiencePage {
+  // Main Layout
+  pageContainer: () => Cypress.Chainable<JQuery<HTMLElement>>;
+  experienceToggle: () => Cypress.Chainable<JQuery<HTMLElement>>;
+
+  // Education Section
+  educationContainer: () => Cypress.Chainable<JQuery<HTMLElement>>;
+  educationInto: () => Cypress.Chainable<JQuery<HTMLElement>>;
+
+  degreeContainer: () => Cypress.Chainable<JQuery<HTMLElement>>;
+  degreeList: () => Cypress.Chainable<JQuery<HTMLElement>>;
+
+  certificateContainer: () => Cypress.Chainable<JQuery<HTMLElement>>;
+  certificationList: () => Cypress.Chainable<JQuery<HTMLElement>>;
+  certificationFilter: () => Cypress.Chainable<JQuery<HTMLElement>>;
+
+  // Work Section
+  workContainer: () => Cypress.Chainable<JQuery<HTMLElement>>;
+  workInto: () => Cypress.Chainable<JQuery<HTMLElement>>; 
+  workToggle: () => Cypress.Chainable<JQuery<HTMLElement>>;
+  workList: () => Cypress.Chainable<JQuery<HTMLElement>>;
+
+
+  constructor() {
+    // Main Layout
+    this.pageContainer = () => {
+      return cy.get("#experienceContainer");
+    };
+    this.experienceToggle = () => { 
+      return cy.get("#experienceToggle");
     }
+
+
+    // Education Section
+    this.educationContainer = () => { 
+      return cy.get("#educationContainer");
+    }
+    this.educationInto = () => { 
+      return cy.get("#educationIntro");
+    }
+
+    this.degreeContainer = () => { 
+      return cy.get("#degreeContainer");
+    }
+    this.degreeList = () => { 
+      return cy.get("#degreeList");
+    }
+
+    this.certificateContainer = () => { 
+      return cy.get("#certificationContainer");
+    }
+    this.certificationList = () => { 
+      return cy.get("#certificationList");
+    }
+    this.certificationFilter = () => {
+      return cy.get("#certificationFilter");
+    }
+
+
+    // Work Section
+    this.workContainer = () => { 
+      return cy.get("#workContainer");
+    }
+    this.workInto = () => { 
+      return cy.get("#workIntro");
+    }
+    this.workToggle = () => { 
+      return cy.get("#workToggle");
+    }
+    this.workList = () => { 
+      return cy.get("#workList");
+    }
+  }
 }
