6fb3533eda8294e51f0ff77b604f0620a8ad16ea
Jesse Greenough <jesse.greenough84@gmail.com>
2023-06-07 13:15:57 -0700

    Update the Devlopers Introduction
    Refactor styles and layout for the Introduction section
    
    Rephrase introduction content

--- components/HomePage/HomeBottom.tsx
+++ components/HomePage/HomeBottom.tsx
@@ -4,13 +4,6 @@ import React, { useEffect, useState } from "react";
 import Image from "next/image";
 
 import Tilt from "react-parallax-tilt";
-import { Row } from "react-bootstrap";
-import {
-  AiFillGithub,
-  AiOutlineTwitter,
-  AiFillInstagram,
-} from "react-icons/ai";
-import { FaLinkedinIn } from "react-icons/fa";
 
 import DeveloperTools from "./DeveloperTools";
 import GithubCard from "./GithubCard";
@@ -28,126 +21,69 @@ const HomeBottom = () => {
   useEffect(() => {
     const checkWindow = () => {
       if (window.innerWidth > 1000) {
-        setWide(true)
+        setWide(true);
+      } else {
+        setWide(false);
       }
-      else {
-        setWide(false)
-      }
-    }
+    };
     checkWindow();
     window.addEventListener("resize", checkWindow);
     return () => {
       window.removeEventListener("resize", checkWindow);
-    }
-  })
+    };
+  });
 
   return (
     <>
-      <Row
+      <section
         id="developerIntro"
-        md={8}
         className={css.developerIntro}
       >
-            <h2>
-              LET ME <span className="detail">INTRODUCE</span> MYSELF
-            </h2>
-            <Tilt className={css.myAvtar}>
-              <Image
-                src={myImg}
-                className={css.myAvatarImage}
-                alt="Human Avatar Icon"
-                width={ 400 }
-                height={ 400 }
-              />
-            </Tilt>
-            <p>
-              I fell in love with programming years ago and have
-              constantly pushed learning more and more at every opportunity! In 2022, one such opportunity that I recieved was the ability to attend the
-              <i>
-                <b className="detail"> Coding Dojo </b>
-              </i>
-              Web Development bootcamp and change my hobby into a full-time career pursuit.
-              <br />
-              <br />
-              I am fluent in
-              <b className="detail"> TypeScript </b> 
-              and 
-              <b className="detail"> JavaScript </b>
-              with significant knowledge of
-              <b className="detail"> Python </b>
-              (with
-              Typings),
-              <b className="detail"> Rust </b>
-              , and 
-              <b className="detail">Java</b>
-              <br />
-              <br />
-              I am passionate about coding because it enables me to leverage technology to create innovative web technologies and products that have a positive
-              <b className="detail"> impact on society. </b>
-              <br />
-              <br />
-              Whenever possible, I apply my passion for developing with
-              <b className="detail"> TypeScript </b>
-              and
-              <b className="detail"> Node.js </b>
-              combined with modern JavaScript libraries and frameworks like
-              <b className="detail"> React.js </b>
-              and
-              <b className="detail"> Next.js</b>
-            </p>
-        </Row>
-        <Languages/>
-        <Framework />
-        <Databases />
-        <DeveloperTools />
-        {wide ? <GithubCard /> : ""}
-        <Row
-          id="developerSocial"
-          md={12}
-          className={css.developerSocial}
-        >
-            <p>
-              Feel free to <span className="detail">connect </span>with me!
-            </p>
-            <ul className={css.developerSocialLinks}>
-              <li>
-                <a
-                  href="https://github.com/GreenJ84"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  <AiFillGithub />
-                </a>
-              </li>
-              <li>
-                <a
-                  href="https://twitter.com/GoodGreens84"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  <AiOutlineTwitter />
-                </a>
-              </li>
-              <li>
-                <a
-                  href="https://www.linkedin.com/in/jessegreenough/"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  <FaLinkedinIn />
-                </a>
-              </li>
-              <li>
-                <a
-                  href="https://www.instagram.com/jesse.greenough/"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  <AiFillInstagram />
-                </a>
-              </li>
-            </ul>
-        </Row>
+        <h2>
+          LET ME <span className="detail">INTRODUCE</span> MYSELF
+        </h2>
+        <Tilt className={css.myAvtar}>
+          <Image
+            src={myImg}
+            className={css.myAvatarImage}
+            alt="Human Avatar Icon"
+            width={400}
+            height={400}
+          />
+        </Tilt>
+        <p>
+          I am Jesse, a Texas born Eagle Scout living in the naturally
+          beautiful, Washington State. I fell in love with programming years ago
+          and have constantly pushed learning more and more at every
+          opportunity! In 2022, one such significant opportunity that I recieved
+          was the ability to attend the
+          <span className="detail_plus"> Coding Dojo </span>
+          Web Development bootcamp and change my side work into a full-time career
+          pursuit.
+          <br />
+          <br />
+          I am passionate about coding because it enables me to leverage
+          technology to create innovative web technologies and products that
+          have a positive
+          <span className="detail_plus"> impact on society. </span>
+          As for tech languages, I am fluent in
+          <span className="detail_plus"> TypeScript </span>
+          and
+          <span className="detail_plus"> JavaScript </span>
+          with
+          <span className="detail_plus"> Node.js </span>
+          and I have a significant knowledge of
+          <span className="detail_plus"> Python </span>
+          (with Typings),
+          <span className="detail_plus"> Rust </span>, and
+          <span className="detail_plus"> Java.</span>
+        </p>
+      </section>
+      <Languages />
+      <Framework />
+      <Databases />
+      <DeveloperTools />
+      {wide ? <GithubCard /> : ""}
     </>
   );
 };
