5f8280f6b3fb1ad7a0ec7e0447e90b23562dfeaf
Jesse Greenough <jesse.greenough84@gmail.com>
2023-06-08 13:27:41 -0700

    Create re-usable Flip Container component

--- components/Layout/FlipCard.tsx
+++ components/Layout/FlipCard.tsx
@@ -1,19 +1,32 @@
 /** @format */
 import React from "react";
+import Tilt from "react-parallax-tilt";
 
-const css = require("../../components/ExperiencePage/Experience.module.css");
+const css = require("./FlipCard.module.css");
 
-
-const FlipCard = ({frontDisplay, backDisplay}: {frontDisplay: JSX.Element, backDisplay: JSX.Element}) => {
+const FlipCard = ({
+  frontDisplay,
+  backDisplay,
+}: {
+  frontDisplay: JSX.Element;
+  backDisplay: JSX.Element;
+}) => {
   return (
     <article className={css.flip_card}>
       <div className={css.flip_card_inner}>
-        <section className={css.flip_card_front}>
-          {frontDisplay}
-        </section>
-        <section className={css.flip_card_back}>
-          {backDisplay}
-        </section>
+        <section className={css.flip_card_front}>{frontDisplay}</section>
+        <Tilt
+          className={css.flip_card_back}
+          tiltReverse={true}
+          tiltMaxAngleX={20}
+          tiltMaxAngleY={30}
+          perspective={700}
+          glareEnable={true}
+          glareBorderRadius="20px"
+        >
+          <section id="flip_card_back">
+            <Tilt tiltEnable={false} perspective={0}>{backDisplay}</Tilt></section>
+        </Tilt>
       </div>
     </article>
   );
