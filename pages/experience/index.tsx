70011f7e6e4361797f139a44ba47fa4763275c5d
Jesse Greenough <jesse.greenough84@gmail.com>
2023-06-08 13:38:26 -0700

    Update the Experience pages
    Refactor page to be a forked link landing page to direct to either independently
    
    Update page to implement interactive UI description cards

--- pages/experience/index.tsx
+++ pages/experience/index.tsx
@@ -5,10 +5,18 @@ import React from "react";
 import Head from "next/head";
 import FlipCard from "../../components/Layout/FlipCard";
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 const css = require("../../components/ExperiencePage/Experience.module.css");
 
 const ExperiencePage = () => {
+  const router = useRouter();
+
+  const handleRedirect = (e: React.MouseEvent<HTMLElement>, url: string) => {
+    e.preventDefault();
+    router.push(url);
+  };
+
   return (
     <>
       <Head>
@@ -32,26 +40,54 @@ const ExperiencePage = () => {
         className={css.experienceContainer}
       >
         <FlipCard
-          frontDisplay={<h1>Education</h1>}
+          frontDisplay={
+            <h1 className={css.cardTitle}>
+              Want to see my <span>Education</span>?
+            </h1>
+          }
           backDisplay={
-            <>
+            <div className={css.cardBody}>
               <p>
                 Learn more about my Personal Educational achievements and
-                persuits
+                persuits. This encompasses both credentials issued from
+                regulated institutions as well as course certifications from
+                various Third-parties.
               </p>
-              <Link href="">Visit</Link>
-            </>
+              <Link
+                href="/experience/education"
+                className={css.experienceLink}
+                onClick={(e: React.MouseEvent<HTMLElement>) => {
+                  handleRedirect(e, "/experience/education");
+                }}
+              >
+                Check it Out!
+              </Link>
+            </div>
           }
         />
         <FlipCard
-          frontDisplay={<h1>Work</h1>}
+          frontDisplay={
+            <h1 className={css.cardTitle} >
+              Want to see my <span>Work</span>?
+            </h1>
+          }
           backDisplay={
-            <>
+            <div className={css.cardBody}>
               <p>
-                Learn more about the Work and Volunter experience I have attianed to date
+                Learn more about the Work and Volunter experience I have
+                partaken in to date. This includes all work experiences across
+                multiple industries.
               </p>
-              <Link href="">Visit</Link>
-            </>
+              <Link
+                className={css.experienceLink}
+                href="/experience/work"
+                onClick={(e: React.MouseEvent<HTMLElement>) => {
+                  handleRedirect(e, "/experience/work");
+                }}
+              >
+                Check it Out!
+              </Link>
+            </div>
           }
         />
       </main>
