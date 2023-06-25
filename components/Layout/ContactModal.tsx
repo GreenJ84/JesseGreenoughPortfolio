2b8eafc1dd9b293195c11cdd10e8f50158f425f2
Jesse Greenough <jesse.greenough84@gmail.com>
2023-06-24 23:20:32 -0700

    Create email contact form with submission

---
+++ components/Layout/ContactModal.tsx
@@ -0,0 +1,83 @@
+/** @format */
+
+import axios from "axios";
+import React, { SyntheticEvent } from "react";
+const css = require("./Footer.module.css");
+
+const ContactModal = ({ close }: { close: Function }) => {
+  async function handleSubmit(form: any) {
+    try {
+      const response = await axios.post('/api/sendEmail', {
+        name: form.senderName.value,
+        sender: form.senderName.value,
+        subject: form.subject.value,
+        message: form.message.value,
+      });
+      console.log(response.data); // Email sent successfully
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  return (
+    <div
+      className={css.emailConnect}
+      onClick={() => close()}
+    >
+      <div>
+        <button
+          className={css.close}
+          onClick={() => close()}
+        >
+          X
+        </button>
+        <h1>Contact Me</h1>
+        <p>
+          Please No solicitations, advertizing, prodect endorsments, sellling,
+          or ill minded offers!
+        </p>
+        <form
+          className={css.contactForm}
+          onSubmit={(e: React.SyntheticEvent) => {
+            console.log('Form submitted');
+            e.preventDefault();
+            handleSubmit(e.target);
+          }}
+        >
+          <label htmlFor="senderName">Name</label>
+          <input
+            name="senderName"
+            type="text"
+            required
+          />
+
+          <label htmlFor="senderEmail">Email</label>
+          <input
+            name="senderEmail"
+            type="text"
+            required
+          />
+
+          <label htmlFor="subject">Subject</label>
+          <input
+            name="subject"
+            type="text"
+            required
+          />
+
+          <label htmlFor="message">Message</label>
+          <textarea
+            name="message"
+            cols={30}
+            rows={6}
+            required
+          ></textarea>
+
+          <button type="submit" >Submit</button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default ContactModal;
