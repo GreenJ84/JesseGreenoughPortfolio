582d4b5f9d4dec69cdd4af6ca32138eb76aab55a
Jesse Greenough <jesse.greenough84@gmail.com>
2023-07-07 11:22:21 -0700

    Page Head Component meta data upates

--- components/Layout/MetaHead.tsx
+++ components/Layout/MetaHead.tsx
@@ -1,30 +1,47 @@
-import Head from 'next/head'
-import React from 'react'
+/** @format */
 
-const MetaHead = ({title, description, keywords }:{title: string, description: string, keywords: string}) => {
+import Head from "next/head";
+import React from "react";
 
+const MetaHead = ({
+  title,
+  description,
+  keywords,
+}: {
+  title: string;
+  description: string;
+  keywords: string;
+}) => {
   return (
     <Head>
-        <title>{title}</title>
-        <meta
-          property="og:title"
-          content={title}
-        />
-        <meta
-          name="description"
-          content={description}
-          key="desc"
-        />
-        <meta
-          property="og:description"
-          content={description}
-        />
-        <meta
-          name="keywords"
-          content={keywords}
-        ></meta>
-      </Head>
-  )
-}
+      <title>{title}</title>
+      <meta
+        property="og:title"
+        content={title}
+      />
+      <meta
+        name="twitter:title"
+        content={title}
+      />
+      <meta
+        name="description"
+        content={description}
+        key="desc"
+      />
+      <meta
+        property="og:description"
+        content={description}
+      />
+      <meta
+        name="twitter:description"
+        content={description}
+      />
+      <meta
+        name="keywords"
+        content={keywords}
+      ></meta>
+    </Head>
+  );
+};
 
-export default MetaHead
+export default MetaHead;
