9fe51df169277f1542fead35edc67689b2be868b
Jesse Greenough <jesse.greenough84@gmail.com>
2023-07-09 23:31:31 -0700

    Create service class for managing all Project DB rerieval functionalities

--- utils/dataTypes.ts
+++ utils/dataTypes.ts
@@ -1,14 +1,12 @@
 /** @format */
 
-import { MongoClient } from "mongodb";
+import { MongoClient, SortDirection, WithId } from "mongodb";
 
 // Database connection
 const DB_CLIENT = new MongoClient(process.env.DB_CONN_STRING!);
 const DB = DB_CLIENT.db(process.env.DB_NAME);
 
-
-export const workDatabase = DB.collection<workType>(process.env.WORK_COLL!);
-export const secWorkDatabase = DB.collection<workType>(process.env.SWORK_COLL!);
+// ======== Work Collection
 export interface workType {
   id?: string;
   company: string;
@@ -20,8 +18,19 @@ export interface workType {
   endDate?: string;
   details: string[];
 }
+export const workDatabase = DB.collection<workType>(process.env.WORK_COLL!);
+export const secWorkDatabase = DB.collection<workType>(process.env.SWORK_COLL!);
+export class workCollectionService {
+  public static async getPrimaryWork() {
+    return await workDatabase.find().sort({ _id: -1 }).limit(5).toArray();
+  }
 
-export const projectDatabase = DB.collection<projectType>(process.env.PROJ_COLL!);
+  public static async getSecondaryWork() {
+    return await secWorkDatabase.find().sort({ _id: -1 }).limit(5).toArray();
+  }
+}
+
+// ======== Project Collection
 export interface projectType {
   id?: string;
   priority: number;
@@ -35,8 +44,104 @@ export interface projectType {
   categories: string[];
   key_techs: string[];
 }
+export const projectDatabase = DB.collection<projectType>(
+  process.env.PROJ_COLL!
+);
+export class projectCollectionService {
+  // Retrieve Project categories and key techs
+  public static async getProjectFilterOptions() {
+    let res: {
+      categories: string[];
+      key_techs: string[];
+    }[] = await projectDatabase
+      .find()
+      .project<{ categories: string[]; key_techs: string[] }>({
+        categories: 1,
+        key_techs: 1,
+        _id: 0,
+      })
+      .toArray();
 
-export const educationDatabase = DB.collection<educationType>(process.env.DEG_COLL!);
+    const categories: string[] = Array.from(
+      new Set(res.map((x) => x.categories).flat())
+    );
+    const techs: string[] = Array.from(
+      new Set(res.map((x) => x.key_techs).flat())
+    );
+
+    return [categories, techs];
+  }
+
+  // Private type mapper
+  static #mapProjectItem(results: WithId<projectType>[]) {
+    return results.map(
+      (result) =>
+        ({
+          id: result._id.toString(),
+          priority: result.priority,
+          name: result.name,
+          date: result.date.slice(0, 7),
+          brief: result.brief ?? result.description,
+          description: result.description,
+          image_path: result.image_path,
+          deployed_url: result.deployed_url,
+          github_url: result.github_url,
+          categories: result.categories,
+          key_techs: result.key_techs,
+        } as projectType)
+    );
+  }
+
+  // Private Project retrival base
+  static async #getProjectItems(
+    sort: boolean = true,
+    offset: number = 0,
+    options?: object
+  ) {
+    let sortOption: { [key: string]: SortDirection } = {
+      name: 1,
+      _id: -1,
+    };
+    if (sort) {
+      (sortOption["date"] = -1), (sortOption["priority"] = 1);
+    }
+
+    return await projectDatabase
+      .find(options || {})
+      .sort(sortOption)
+      .skip(offset)
+      .limit(10)
+      .toArray();
+  }
+
+  public async getUnsortedProjects(offset: number = 0) {
+    return projectCollectionService.#mapProjectItem(await projectCollectionService.#getProjectItems(false, offset));
+  }
+
+  // Top priority project retrival
+  public static async getTopProjects() {
+    return this.#mapProjectItem(await this.#getProjectItems());
+  }
+
+  // Category filtered project retrival
+  public async getProjectsByCategory(category: string, offset: number = 0) {
+    return projectCollectionService.#mapProjectItem(
+      await projectCollectionService.#getProjectItems(true, offset, { categories: category })
+    );
+  }
+
+  // Key tech filtered project retrival
+  public async getProjectsByTech(tech: string, offset: number = 0) {
+    return projectCollectionService.#mapProjectItem(
+      await projectCollectionService.#getProjectItems(true, offset, { key_techs: tech })
+    );
+  }
+}
+
+// ======== Education Collection
+export const educationDatabase = DB.collection<educationType>(
+  process.env.DEG_COLL!
+);
 export interface educationType {
   id?: string;
   college: string;
@@ -49,7 +154,9 @@ export interface educationType {
   website: string;
 }
 
-export const certificationDatabase = DB.collection<certificationType>(process.env.CERT_COLL!);
+export const certificationDatabase = DB.collection<certificationType>(
+  process.env.CERT_COLL!
+);
 export interface certificationType {
   id?: string;
   priority: number;
