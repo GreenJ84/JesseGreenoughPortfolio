077131abe7d2260fc03ac6c5904d8e8d38586f6b
Jesse Greenough <jesse.greenough84@gmail.com>
2023-07-10 21:01:24 -0700

    Create Database service for certification items
    Implement functionality for mapping databse items
    
    Implement functionality for all types of certification data rerievals

--- utils/dataTypes.ts
+++ utils/dataTypes.ts
@@ -1,6 +1,6 @@
 /** @format */
 
-import { Collection, MongoClient, SortDirection, WithId } from "mongodb";
+import { MongoClient, SortDirection, WithId } from "mongodb";
 
 // Database connection
 const DB_CLIENT = new MongoClient(process.env.DB_CONN_STRING!);
@@ -24,15 +24,15 @@ export class workCollectionService {
   static #mapWorkData(data: WithId<workType>[]) {
     return data.map(
       (result) =>
-      ({
-        id: result._id.toString(),
-        company: result.company,
-        logo: result.logo,
-        position: result.position,
-        location: result.location,
-        date: result.date,
-        details: result.details,
-      } as workType)
+        ({
+          id: result._id.toString(),
+          company: result.company,
+          logo: result.logo,
+          position: result.position,
+          location: result.location,
+          date: result.date,
+          details: result.details,
+        } as workType)
     );
   }
 
@@ -40,7 +40,7 @@ export class workCollectionService {
     const workTotal = await workDatabase.countDocuments();
     const secWorkTotal = await secWorkDatabase.countDocuments();
 
-    return [ workTotal, secWorkTotal ];
+    return [workTotal, secWorkTotal];
   }
 
   public async getPrimaryWork(offset: number = 0) {
@@ -147,7 +147,7 @@ export class projectCollectionService {
   static async #getProjectItems(
     sort: boolean = true,
     offset: number = 0,
-    options?: object
+    filterOptions?: object
   ) {
     let sortOption: { [key: string]: SortDirection } = {
       name: 1,
@@ -162,7 +162,7 @@ export class projectCollectionService {
     }
 
     return await projectDatabase
-      .find(options || {})
+      .find(filterOptions ?? {})
       .sort(sortOption)
       .skip(offset)
       .limit(10)
@@ -218,6 +218,7 @@ export interface educationType {
   website: string;
 }
 
+// ======== Certification Collection
 export const certificationDatabase = DB.collection<certificationType>(
   process.env.CERT_COLL!
 );
@@ -231,6 +232,117 @@ export interface certificationType {
   image: string;
   url: string;
   techs: string[];
+}
+export class certificationCollectionService {
+  public static async getCertificationFilterOptions(): Promise<[string, string]> { 
+    let res: {
+      issuer: string;
+      techs: string[];
+    }[] = await certificationDatabase
+      .find()
+        .project<{ issuer: string; techs: string[]; }>({
+        issuer: 1,
+        techs: 1,
+        _id: 0,
+      })
+      .toArray();
+
+    const issuerMap = new Map<string, number>();
+    const techMap = new Map<string, number>();
+    res.map((item) => {
+      if (issuerMap.has(item.issuer)) {
+        issuerMap.set(item.issuer, issuerMap.get(item.issuer)! + 1);
+      } else {
+        issuerMap.set(item.issuer, 1);
+      }
+      item.techs.forEach((tech) => {
+        if (techMap.has(tech)) {
+          techMap.set(tech, techMap.get(tech)! + 1);
+        } else {
+          techMap.set(tech, 1);
+        }
+      });
+    });
+
+    return [
+      JSON.stringify(Array.from(issuerMap.entries())),
+      JSON.stringify(Array.from(techMap.entries())),
+    ];
+  }
+
+  static #mapCertificationData(data: WithId<certificationType>[]) {
+    return data.map(
+      (result) =>
+        ({
+          id: result._id.toString(),
+          priority: result.priority,
+          title: result.title,
+          issuer: result.issuer,
+          date: result.date.slice(0, 10),
+          description: result.description,
+          image: result.image,
+          url: result.url,
+          techs: result.techs,
+        } as certificationType)
+    );
+  }
+
+  static async #getCertificationItems(
+    sort: boolean = true,
+    offset: number = 0,
+    filterOptions?: object
+  ) {
+    let sortOption: { [key: string]: SortDirection } = {
+      title: 1,
+      _id: -1,
+    };
+    if (sort) {
+      sortOption = {
+        priority: 1,
+        date: -1,
+        ...sortOption,
+      };
+    }
+    return this.#mapCertificationData(
+      await certificationDatabase
+        .find(filterOptions ?? {})
+        .sort({})
+        .skip(offset)
+        .limit(10)
+        .toArray()
+    );
+  }
+
+  public async getUnsortedCertification(offset: number = 0) {
+    return await certificationCollectionService.#getCertificationItems(
+      false,
+      offset
+    );
+  }
+
+  static async getTopCertification(): Promise<[certificationType[], number]> {
+    return [await certificationCollectionService.#getCertificationItems(), await certificationDatabase.countDocuments()];
+  }
+
+  public async getCertificationByTech(tech: string, offset: number = 0) {
+    return await certificationCollectionService.#getCertificationItems(
+      true,
+      offset,
+      {
+        techs: tech,
+      }
+    );
+  }
+
+  public async getCertificationByIssuer(issuer: string, offset: number = 0) {
+    return await certificationCollectionService.#getCertificationItems(
+      true,
+      offset,
+      {
+        issuer: issuer,
+      }
+    );
+  }
 }
 
 export const resumeDatabase = DB.collection<resumeType>(process.env.RES_COLL!);
