bd0628cb871cca26c7f29d156775fba32ea9dc40
Jesse Greenough <jesse.greenough84@gmail.com>
2023-07-18 23:51:09 -0700

    Accissability updates

--- components/Layout/LayoutExtras.tsx
+++ components/Layout/LayoutExtras.tsx
@@ -227,15 +227,20 @@ export const AddItemButton = ({
   clickHandler: React.MouseEventHandler;
 }) => {
   return (
-    <button
-      name="add_item_button"
-      onClick={clickHandler}
-      className={css.add_item_button}
+    <li
+      id="AddItem"
+      aria-label={`Load More ${itemType} Items`}
     >
-      <span>Load More {itemType}</span>
-      <BsPlusCircleFill className={css.addIcon} />
-    </button>
-);
+      <button
+        aria-labelledby="AddItem"
+        onClick={clickHandler}
+        className={css.add_item_button}
+      >
+        <span>Load More {itemType}</span>
+        <BsPlusCircleFill className={css.addIcon} />
+      </button>
+    </li>
+  );
 };
 
 // Typwriter animtation
