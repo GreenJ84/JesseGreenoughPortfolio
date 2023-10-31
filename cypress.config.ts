e5c8ca1896b2a8a80e213394e7621ac010bcbbf5
Jesse Greenough <jesse.greenough84@gmail.com>
2023-10-31 10:46:13 -0700

    Update configuration files for app upgrade

--- cypress.config.ts
+++ cypress.config.ts
@@ -8,7 +8,6 @@ export default defineConfig({
   responseTimeout: 5000,
   pageLoadTimeout: 10000,
   video: true,
-  videoUploadOnPasses: false,
   videosFolder: "cypress/videos",
   screenshotsFolder: "cypress/screenshots",
   fixturesFolder: "cypress/fixtures",
@@ -26,8 +25,8 @@ export default defineConfig({
       // implement node event listeners here
     },
     baseUrl: "http://localhost:3000/",
-    specPattern: "cypress/e2e/**/*.cy.{ts,tsx}",
-    excludeSpecPattern: "cypress/e2e/examples/*.cy.{ts,tsx}",
+    specPattern: "src/cypress/e2e/**/*.cy.{ts,tsx}",
+    excludeSpecPattern: "src/cypress/e2e/examples/*.cy.{ts,tsx}",
     slowTestThreshold: 10000,
     testIsolation: false,
   },
