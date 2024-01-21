ac6b27f3f3ddd7e52e858fd8a77d52d437f5fd30
Jesse Greenough <jesse.greenough84@gmail.com>
2024-01-20 23:59:20 -0800

    Complete Beta Navigation Bar upgrade

---
+++ src/app/_components/Layout/Navbar/ThemeSwitch.tsx
@@ -0,0 +1,37 @@
+/** @format */
+'use client';
+import React, { useContext } from 'react';
+
+import { AppContext } from '../../../_utils/AppContext';
+
+const css = require('./Navbar.module.css');
+
+const ThemeSwitch = () => {
+  const { theme, setTheme } = useContext(AppContext);
+
+  return (
+    <label
+      aria-label='Toggle Day and Night themes'
+      className={css.themeSwitch}
+    >
+      <input
+        id='themeChange'
+        aria-labelledby='themeChange'
+        className={css.themeInput}
+        type='checkbox'
+        aria-checked={theme === 'dark'}
+        defaultChecked={theme === 'dark'}
+        data-theme-toggle
+        onClick={() => {
+          setTheme();
+        }}
+      />
+      <span
+        aria-controls='themeChange'
+        className={css.themeSlider}
+      ></span>
+    </label>
+  );
+};
+
+export default ThemeSwitch;
