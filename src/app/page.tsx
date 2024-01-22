8beb0f1214a5fa0e92335d042ba383cd7edbbbc6
Jesse Greenough <jesse.greenough84@gmail.com>
2024-01-22 01:50:56 -0800

    Complete file system for app routining structure
    All blank page components

---
+++ src/app/page.tsx
@@ -0,0 +1,18 @@
+/** @format */
+
+'use client';
+import React from "react";
+
+const css = require("./_components/HomePage/Home.module.css");
+
+const HomePage = () => {
+  return (
+    <>
+      <main id="homePage" className={css.homePage}>
+
+      </main>
+    </>
+  );
+};
+
+export default HomePage;
