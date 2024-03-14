f76de59fbed9711aa567fe18bf8b7707d89e1e35
Jesse Greenough <jesse.greenough84@gmail.com>
2024-03-14 02:18:24 -0700

    Modularize Layout Links and Icons

---
+++ src/app/_components/Layout/Shared/SocialLinks.ts
@@ -0,0 +1,36 @@
+import { AiFillGithub, AiOutlineTwitter, AiFillInstagram} from "react-icons/ai";
+import { FaLinkedinIn } from "react-icons/fa";
+import { NavLink } from './NavigationLinks';
+
+
+export const SocialIcons = {
+  Github: AiFillGithub,
+  Twitter: AiOutlineTwitter,
+  Instagram: AiFillInstagram,
+  Linkedin: FaLinkedinIn,
+}
+
+const SocialNavLinks: NavLink[] = [
+  {
+    route: "https://github.com/GreenJ84",
+    text: "GitHub",
+    icon: SocialIcons.Github,
+  },
+  {
+    route: "https://twitter.com/GoodGreens84",
+    text: "Twitter",
+    icon: SocialIcons.Twitter,
+  },
+  {
+    route: "https://www.linkedin.com/in/jessegreenough/",
+    text: "LinkedIn",
+    icon: SocialIcons.Linkedin,
+  },
+  {
+    route: "https://www.instagram.com/jesse.greenough/",
+    text: "Instagram",
+    icon: SocialIcons.Instagram,
+  },
+];
+
+export default SocialNavLinks;
