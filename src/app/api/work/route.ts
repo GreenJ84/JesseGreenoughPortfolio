2434a5d488dad507704a53c3176ad99f95489029
Jesse Greenough <jesse.greenough84@gmail.com>
2024-12-19 22:56:31 -0800

    Migrate work api route into new app directory - Update file for directory conventions

---
+++ src/app/api/work/route.ts
@@ -0,0 +1,34 @@
+/** @format */
+
+import { NextRequest, NextResponse } from "next/server";
+
+import * as workService from "../../work/workService";
+
+export async function GET(
+  req: NextRequest
+) {
+  const searchParams = req.nextUrl.searchParams;
+  let type = searchParams.get('type');
+  if (!type) type = "primary"
+
+  let offset = 0;
+  const offsetParam = searchParams.get('offset');
+  if (!!offsetParam){
+    try { offset = parseInt(offsetParam); }
+    catch {}
+  }
+
+  let results: workService.workType[] = [];
+  switch (type) {
+    case "primary":
+      results = await workService.getPrimaryWork(offset);
+      break;
+    case "secondary":
+      results = await workService.getSecondaryWork(offset);
+      break;
+    default:
+      return NextResponse.json("Not Found", { status: 500 });
+  }
+
+  return NextResponse.json(results, { status: 200 });
+}
