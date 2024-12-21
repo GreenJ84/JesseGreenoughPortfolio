039846226dd2fc87b8f757fc2880bca090713fff
Jesse Greenough <jesse.greenough84@gmail.com>
2024-12-21 01:38:03 -0800

    Implement Work page Metadata export

--- src/app/work/page.tsx
+++ src/app/work/page.tsx
@@ -1,5 +1,5 @@
 /** @format */
-// TODO: Migrate to app directory
+// TODO: Update styling logic for route
 import React from "react";
 
 import * as workService from "./workService";
@@ -14,11 +14,6 @@ const WorkPage = async () => {
     workService.getPrimaryWork(),
     workService.getWorkTotals(),
   ]);
-  {/* <MetaHead
-    title="Lifetime Work Experience for Jesse Greenough"
-    description="View the Work experience attained by Jesse Greenough of his life"
-    keywords="Work,Experience,Management,Software,Development,Engineering,Retail"
-  /> */}
   return (
     <main
       id="workContainer"
@@ -30,3 +25,32 @@ const WorkPage = async () => {
 };
 
 export default WorkPage;
+
+
+import { Metadata } from 'next/types';
+import { APP_URL, OPEN_GRAPH, TWITTER_SHARE } from '../sharedMetadata';
+
+const WORK_URL = APP_URL + '/work';
+const TITLE="Lifetime Work Experience for Jesse Greenough"
+const DESCRIPTION ="View the Work experience attained by Jesse Greenough over his lifetime"
+const KEYWORDS ="Work,Experience,Management,Software,Development,Engineering,Retail"
+
+export const metadata: Metadata = {
+  metadataBase: new URL(WORK_URL),
+  title: TITLE,
+  description: DESCRIPTION,
+  keywords: KEYWORDS,
+  openGraph: {
+    title: TITLE,
+    description: DESCRIPTION,
+    url: WORK_URL,
+    siteName: TITLE,
+    ...OPEN_GRAPH
+  },
+  twitter: {
+    site: WORK_URL,
+    title: TITLE,
+    description: DESCRIPTION,
+    ...TWITTER_SHARE
+  },
+};
