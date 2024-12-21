8c9a3c053556ad16ddaa3feb2f69ebff0ef33053
Jesse Greenough <jesse.greenough84@gmail.com>
2024-12-21 01:37:00 -0800

    Implement Education page Metadata export

--- src/app/education/page.tsx
+++ src/app/education/page.tsx
@@ -1,5 +1,6 @@
 /** @format */
 
+// TODO: Update styling logic gor route
 import React from "react";
 import dynamic from "next/dynamic";
 
@@ -60,3 +61,32 @@ const Page = async () => {
 };
 
 export default Page;
+
+
+import { Metadata } from 'next/types';
+import { APP_URL, OPEN_GRAPH, TWITTER_SHARE } from '../sharedMetadata';
+
+const EDU_URL = APP_URL + '/education';
+const TITLE="Lifetime Educational Experience for Jesse Greenough"
+const DESCRIPTION ="View the scholarly efforts undergone by Jesse Greenough over his lifetime"
+const KEYWORDS ="Education,Experience,Coursework,Software,Development,Engineering"
+
+export const metadata: Metadata = {
+  metadataBase: new URL(EDU_URL),
+  title: TITLE,
+  description: DESCRIPTION,
+  keywords: KEYWORDS,
+  openGraph: {
+    title: TITLE,
+    description: DESCRIPTION,
+    url: EDU_URL,
+    siteName: TITLE,
+    ...OPEN_GRAPH
+  },
+  twitter: {
+    site: EDU_URL,
+    title: TITLE,
+    description: DESCRIPTION,
+    ...TWITTER_SHARE
+  },
+};
