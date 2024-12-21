74e159296406b362c48f8ed9c1a205515bb18043
Jesse Greenough <jesse.greenough84@gmail.com>
2024-12-21 01:37:28 -0800

    Implement Resumes page Metadata export

--- src/app/resumes/page.tsx
+++ src/app/resumes/page.tsx
@@ -1,4 +1,7 @@
 /** @format */
+// TODO: Update styling logic gor route
+import React from "react";
+
 import * as resumeService from "./resumeService";
 
 import ResumeClient from "./ResumeClient";
@@ -11,12 +14,6 @@ const ResumePage = async () => {
     resumeService.getResumeCount(),
     resumeService.getResumeFilterOptions(),
   ]);
-
-  {/* <MetaHead
-    title="Jesse Greenough's Software Engineering Resumes"
-    description="View and Download Jesse Greenough's Software Engineering Resumes"
-    keywords="Resume,Full-Stack,Software,Developer,Engineer,TypeScript,React,NextJS,Python,Java,Rust"
-  /> */}
   return (
       <main
         id="resumePage"
@@ -32,3 +29,32 @@ const ResumePage = async () => {
 };
 
 export default ResumePage;
+
+
+import { Metadata } from 'next/types';
+import { APP_URL, OPEN_GRAPH, TWITTER_SHARE } from '../sharedMetadata';
+
+const RESUME_URL = APP_URL + '/resumes';
+const TITLE="Jesse Greenough's Software Engineering Resumes"
+const DESCRIPTION ="View and Download Jesse Greenough's Software Engineering Resumes"
+const KEYWORDS ="Resume,Full-Stack,Software,Developer,Engineer,TypeScript,React,NextJS,Java,Rust,Python"
+
+export const metadata: Metadata = {
+  metadataBase: new URL(RESUME_URL),
+  title: TITLE,
+  description: DESCRIPTION,
+  keywords: KEYWORDS,
+  openGraph: {
+    title: TITLE,
+    description: DESCRIPTION,
+    url: RESUME_URL,
+    siteName: TITLE,
+    ...OPEN_GRAPH
+  },
+  twitter: {
+    site: RESUME_URL,
+    title: TITLE,
+    description: DESCRIPTION,
+    ...TWITTER_SHARE
+  },
+};
