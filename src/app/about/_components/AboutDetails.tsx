f5eaa459a99db47aab2b3ea3a62ea3ea108af324
Jesse Greenough <jesse.greenough84@gmail.com>
2024-12-28 00:57:29 -0800

    Update about page content - Refresh content for more relevance - Add newer life events - Update stylings

--- src/app/about/_components/AboutDetails.tsx
+++ src/app/about/_components/AboutDetails.tsx
@@ -2,50 +2,50 @@
 
 import React from "react";
 
+import { DeveloperPortrait } from "../../_layout/LayoutExtras";
+
 const css = require("./About.module.css");
 
 const AboutDetails = () => {
   return (<>
     <section
-      id="aboutDetail"
       className={css.aboutDetail}
     >
       <h2>
         All About <span className="detail">Me</span>
       </h2>
+      <DeveloperPortrait />
       <p>
-        {" "}
         I was born and raised in
-        <span className="detail"> El Paso, Texas, </span>a border city known for
+        <span className="detail"> El Paso, Texas</span>, a border city known for
         its unique cultural blend. Ever since I was young, I was driven to
         achieve personal growth and make the most of my potential. I actively
         participated in community sports, educational clubs (math club, debate
         team, etc), and the Boy Scouts of America. These activities instilled in
         me important values of leadership, resilience, and community service. Of
-        which is also where my fascination with nature (GREEN-ery =&gt;
-        GREEN-ough 🤣) and my love of math began.
+        which is also where my fascination with nature and my love of math began.
       </p>
     </section>
     <section
-      id="aboutDetail"
       className={css.aboutDetail}
     >
       <h2>
-        <span className="detail">Early</span> Education
+        Early <span className="detail">Education</span>
       </h2>
       <p>
-        {" "}
-        In high school, I attended a specialized Running Start program where
-        through my determination and effort I attained distinguished
-        achievements and earned my Associates degree in my junior year of high
-        school, a year earlier than the program (1 of 3 students to accomplish
-        that feat for the city of El Paso that year). It was during this time
-        that I discovered my passion for coding through the robotics club, which
-        sparked my interest in the world of programming and ignited my curiosity
-        to explore the endless possibilities of software development.
+        I had the opportunity to attend a specialized dual credit early college high school that was relatively new within my hometown. It was this school that allowed me, with my determination and effort, to earn my Associates degree in my junior year of high school, a year earlier than the program (1 of 3 students to accomplish that feat within my graduating class).
       </p>
+      <p style={{"marginTop": "8dvh"}}>
+        I was able to attend the University of Texas - El Paso during my senior year and upon graduating I had attained distinguished achievement and honors. It was during this period time that I discovered my passion for coding through the robotics, which sparked my interest in the world of programming and ignited my curiosity to explore the endless possibilities of software development.
+      </p>
+    </section>
+    <section
+      className={css.aboutDetail}
+    >
+      <h2>
+        <span className="detail">College</span> Transition
+      </h2>
       <p>
-        {" "}
         Transitioning to college at the University of Washington Seattle campus
         was one of the biggest challenges of my life as I adapted to a new
         environment across the country away from the only home I had known. In
@@ -54,43 +54,45 @@ const AboutDetails = () => {
         altered my perception of reality, affected my own mental well-being, and
         distorted my view of the people I knew and the events of my childhood.
         This was a profound personal challenge that tested my resilience and
-        forced me to reevaluate my priorities. As a result, I had to withdraw
+        forced me to reevaluate my priorities. As a result, I chose to withdraw
         from college to navigate my own mental health crisis, taking a
         courageous step towards prioritizing self-care and seeking the support I
         needed.
       </p>
     </section>
     <section
-      id="aboutDetail"
       className={css.aboutDetail}
     >
       <h2>
-        <span className="detail">Career</span> and <span className="detail">Family</span>
+        Early <span className="detail">Career</span>
       </h2>
       <p>
-        {" "}
-        From there I re-established myself and entered the workforce as my only
-        foreseeable means to pay off accumulated debts. Embracing a structured
+        After re-establishing myself upon leaving college, I entered the workforce. Embracing a structured
         environment, I tapped into my strong work ethic and adaptability to
         thrive in various roles. This allowed me to progress from entry-level
         positions to leadership roles such as Store Apprentice at Chipotle and
         Department Manager at Fred Meyer, where I honed my managerial skills,
-        fostered a positive team culture, and contributed to the growth of the
-        organization(s) for over 8 years. It was during my time as a Fred Meyer Department manager that I met
-        my incredible fiancé and became a proud father to a beautiful daughter and handsome son
-        who inspire me to be the best version of myself and motivate me to
-        create a brighter future.
+        fostered positive team cultures, and contributed to the customer experience of the
+        service industry for over 8 years.
       </p>
     </section>
     <section
-      id="aboutDetail"
       className={css.aboutDetail}
     >
       <h2>
-        Return to <span className="detail">Education</span>
+        Start of a <span className="detail">Family</span>
       </h2>
       <p>
-        {" "}
+        It was during my time as a Fred Meyer Department manager that I met an incredible woman and with her, an amazing daughter. With time I was able to leech onto those two and become a proud husband and even prouder father. Not soon after, my wife and I had a handsome son come into this world as well. My children inspire me to be the best version of myself and motivate me to create a brighter future.
+      </p>
+    </section>
+    <section
+      className={css.aboutDetail}
+    >
+      <h2>
+        <span className="detail">Coding</span> Bootcamp
+      </h2>
+      <p>
         While balancing work and life, I continued to expand my coding knowledge
         during my free time, embracing coding tutorials, online resources, and
         personal projects which allowed me to explore different languages and
@@ -98,28 +100,38 @@ const AboutDetails = () => {
         in 2022, I received the opportunity to attend the Coding Dojo full stack
         web development bootcamp. Within the bootcamp I further honed my skills
         and acquired the necessary expertise to become a versatile full-stack
-        software developer and ever since I knew this was the career field I wanted to be a part of.
-        I had then dedicated my free time to continuous learning,
+        software developer.
+        Even after the camp I dedicated my free time to continuous learning,
         actively engaging in coding challenges, collaborating on real-world projects,
         and planning for return to formal education.
       </p>
+    </section>
+    <section
+      className={css.aboutDetail}
+    >
+      <h2>
+        Return to <span className="detail">Education</span>
+      </h2>
       <p>
-        In 2024, I completed my Bachelor of Science in Software Engineering from Western Governors University,
-        and I plan to return in the future for my Masters degree. Above all else, I will always strive to
-        enhance my abilities in the ever-evolving field of software development.
+        In 2024, I proudly earned my Bachelor of Science in Software Engineering from Western Governors University (WGU), a milestone that marked the culmination of dedication, perseverance, and passion for the field. My undergraduate journey provided me with a solid foundation in software development, equipping me with both the theoretical knowledge and practical skills to thrive in the dynamic world of technology.
       </p>
-  </section>
-  <section
-    id="aboutDetail"
-    className={css.aboutDetail}
-  >
       <p>
+        Recognizing the importance of continuous growth in an industry that never stands still, I decided to return to WGU the following year to pursue a Master of Science in Cybersecurity. This decision reflects my commitment to expanding my expertise into the critical realm of protecting digital systems and safeguarding information in an increasingly interconnected world. By building upon my software engineering background with advanced studies in cybersecurity, I aim to become a well-rounded professional capable of addressing complex challenges and driving innovation in both fields.
+      </p>
+      <p>
+        Above all, I am driven by a relentless desire to learn and evolve, ensuring that I stay at the forefront of the ever-changing landscape of technology. Whether it's mastering the latest programming techniques or tackling cutting-edge security issues, I am passionate about pushing boundaries and making a meaningful impact in the tech community.
+      </p>
+    </section>
+    <section
+      className={css.aboutDetail}
+    >
+      <p style={{"textAlign": "center"}}>
         Through my journey, I have experienced personal and professional growth,
         and I am eager to leverage my skills and passion for coding to
-        contribute to the dynamic world of software development. With a firm
-        belief in the power of technology to drive positive change, I am excited
-        to be part of innovative projects that shape the future and make a
-        meaningful impact in the lives of others.
+        contribute to the dynamic world of software development. I am driven by a relentless desire to learn and evolve, ensuring that I stay at the forefront of the ever-changing landscape of technology. Whether it's mastering the latest programming techniques or tackling cutting-edge security issues, I am passionate about pushing boundaries and making a meaningful impact in the tech community. 
+      </p>
+      <p style={{"textAlign": "center", "fontSize" : "var(--header2)", "marginTop": "10dvh"}}>
+        With a firm belief in the power of technology to drive positive change, I am excited to be part of innovative projects that shape the future and make a meaningful impact in the lives of others.
       </p>
     </section>
   </>);
