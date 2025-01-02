895a388226549295e00132659e3c97c4cf05255a
Jesse Greenough <jesse.greenough84@gmail.com>
2025-01-01 23:35:48 -0800

    Update page component: - Add classes to components - Add a Scroll Nav that needs implementation

--- src/app/skills/page.tsx
+++ src/app/skills/page.tsx
@@ -1,32 +1,41 @@
 // TODO: Update styling logic gor route
 import React from 'react';
+import dynamic from 'next/dynamic';
 
 import { skillData } from './_components/skillData';
 import TechnicalSkill from './_components/TechnicalSkill';
+import SkillsNav from './_components/SkillsNav';
+const Footer = dynamic(() => import("../_layout/Footer/Footer"));
+
 
 const css = require("./_components/Skill.module.css");
 
 const page = () => {
-  const formattedName = (name: string) => name.charAt(0).toUpperCase() + name.slice(1).split("_").join(" ");
 
   return (
-    <>
-        {Object.entries(skillData).map(
-          ([category, data]) =>{
-            return <section key={category}>
-              <h2>{formattedName(category)}</h2>
-              <ul
-                id={`${category}`}
-                className={css.skillsListContainer}
-              >
-                {data.map(skill => {
-                  return <TechnicalSkill key={skill.name} {...skill}/>
-                })}
-              </ul>
-            </section>
-          })}
-    </>
-  )
+    <main
+      className="scrollSnapContainer"
+    >
+      <SkillsNav sections={Object.keys(skillData)}/>
+      {Object.entries(skillData).map(
+        ([category, data]) =>{
+          return <section
+            id={category}
+            key={category}
+            className={`${css.skillSection} skillSection`}
+          >
+            <ul
+              className={css.skillsListContainer}
+            >
+              {data.map(skill =>
+                <TechnicalSkill key={skill.name} {...skill}/>
+              )}
+            </ul>
+          </section>
+      })}
+      <Footer />
+    </main>
+  );
 }
 
 export default page;
@@ -38,6 +47,7 @@ const KEYWORDS="Full-Stack,Software,Developer,Engineer,TypeScript,React,NextJS,J
 
 import { APP_URL, OPEN_GRAPH, TWITTER_SHARE } from '../sharedMetadata';
 import { Metadata } from 'next/types';
+
 export const metadata: Metadata = {
   metadataBase: new URL(SKILLS_URL),
   title: TITLE,
