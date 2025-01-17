e2b8dc15690813430def445cf7e10bf8cd72e0a6
Jesse Greenough <jesse.greenough84@gmail.com>
2025-01-17 03:12:14 -0800

    Migrate ProjectDetailCard: - Create a new file to hold the component implementation for the Project Detailed overlay modal - Migrate all related logic, structure, and imports to the new file - Implement newly created component within the ProjectCard

--- src/app/projects/_components/ProjectCard.tsx
+++ src/app/projects/_components/ProjectCard.tsx
@@ -2,21 +2,13 @@
 
 "use client";
 
-import dynamic from "next/dynamic";
 import React, { useState } from "react";
-
-const AiFillGithub = dynamic(() =>
-  import("react-icons/ai").then((m) => m.AiFillGithub)
-);
-const AiFillProject = dynamic(() =>
-  import("react-icons/ai").then((m) => m.AiFillProject)
-);
-const MdClose = dynamic(() => import("react-icons/md").then((m) => m.MdClose));
 
 import Image from "next/image";
 import FlipCard from "../../_shared/FlipCard";
 
 import { projectType } from "../projectService";
+import ProjectDetailCard from "./ProjectDetailCard";
 
 const css = require("./Project.module.css");
 
@@ -29,7 +21,6 @@ const ProjectCard = ({ project }: { project: projectType }) => {
         style={{ width: "100%", height: "100%", margin: "0", padding: "0" }}
         frontDisplay={
           <div
-            id="projectDisplayCard"
             className={css.projectDisplay}
           >
             <Image
@@ -37,7 +28,7 @@ const ProjectCard = ({ project }: { project: projectType }) => {
               src={project.image_path}
               alt={project.name}
               className={css.displayImage}
-              height="150"
+              height="200"
               width="300"
             />
             <p className={css.displayTitle}>{project.name}</p>
@@ -45,8 +36,7 @@ const ProjectCard = ({ project }: { project: projectType }) => {
         }
         backDisplay={
           <div
-            id="projectDetailCard"
-            className={css.projectDetail}
+            className={css.displayDetail}
           >
             <h2>{project.name}</h2>
             <p>{project.brief}</p>
@@ -56,68 +46,14 @@ const ProjectCard = ({ project }: { project: projectType }) => {
       />
 
       {showDetail && (
-        <div className={css.detailModal}>
-          <div
-            id="modalUnderlay"
-            className={css.cover}
-            onClick={() => setShowDetail(false)}
-          ></div>
-          <dialog
-            id="projectDetailModal"
-            className={css.detail}
-          >
-            <div
-              id="detailLeft"
-              className={css.detailLeft}
-            >
-              <Image
-                src={project.image_path}
-                alt={project.name}
-                className={css.detailImage}
-                height="150"
-                width="300"
-              />
-              <div id="projectLinks">
-                <a href={project.github_url}>
-                  <AiFillGithub className={css.detailIcons} />
-                  <span>Github</span>
-                </a>
-                {project.deployed_url && (
-                  <a href={project.deployed_url}>
-                    <AiFillProject className={css.detailIcons} />
-                    <span>Project</span>
-                  </a>
-                )}
-              </div>
-            </div>
-
-            <div
-              id="detailRight"
-              className={css.detailRight}
-            >
-              <h2>{project.name}</h2>
-              <p>{project.description}</p>
-              <h3>Key Techs:</h3>
-              <ul className={css.techs}>
-                {project.key_techs.map((tech) => (
-                  <li
-                    key={tech}
-                    className={css.detailTech}
-                  >
-                    {tech}
-                  </li>
-                ))}
-              </ul>
-            </div>
-
-            <button
-              id="modalClose"
-              onClick={() => setShowDetail(false)}
-              className={css.detailClose}
-            >
-              <MdClose />
-            </button>
-          </dialog>
+        <div
+          className={css.detailModal}
+          onClick={() => setShowDetail(false)}
+        >
+          <ProjectDetailCard
+            project={project}
+            setShowDetail={setShowDetail}
+          />
         </div>
       )}
     </li>
