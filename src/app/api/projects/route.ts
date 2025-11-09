ac0362f009d1e2919fba30560fcd590af2ec10aa
Jesse Greenough <jesse.greenough84@gmail.com>
2025-11-09 01:37:57 -0800

    Update Projects api route: - Implement default values for parameters - Implement better integer parsing - Implement better error handling

--- src/app/api/projects/route.ts
+++ src/app/api/projects/route.ts
@@ -1,39 +1,41 @@
 import { NextRequest, NextResponse } from "next/server";
 
-import { getProjectsByCategory, getProjectsByTech, getTopProjects, getAllProjects, projectType } from "../../(portfolio)/projects/projectService";
+import { ProjectType } from "@/app/_lib/_types";
+import { getProjectsByCategory, getProjectsByTech, getAllProjects } from "@/app/_actions/projectService";
 
 export async function GET (req: NextRequest){
   const searchParams = req.nextUrl.searchParams;
-  const type = searchParams.get('type');
-  if (!type) return NextResponse.json(await getTopProjects(), {status: 200});
+  const type = searchParams.get('type') ?? 'all';
 
-  let offset = 0;
-  let offsetParam = searchParams.get('offset');
-  if (!!offsetParam){
-    try { offset = parseInt(offsetParam); }
-    catch {}
-  }
+  const offsetRaw = searchParams.get("offset") ?? "0";
+  const offset = Number.isNaN(Number(offsetRaw)) ? 0 : parseInt(offsetRaw, 10);
+
   const filter = searchParams.get('filter');
 
-  let results: projectType[] = [];
-  switch (type) {
-    case "all":
-      results = await getAllProjects(offset);
-      break;
-    case "category":
-      if (!filter) {
-        return NextResponse.json("Requesting projects of a certain category requires the category filter", {status: 404});
-      }
-      results = await getProjectsByCategory(filter!, offset);
-      break;
-    case "tech":
-      if (!filter) {
-        return NextResponse.json("Requesting projects of a certain tech requires the tech filter", {status: 404});
-      }
-      results = await getProjectsByTech(filter!, offset);
-      break;
-    default:
-      return NextResponse.json("Server cannot handle untyped request", {status: 500});
+  try {
+    let results: ProjectType[] = [];
+    switch (type) {
+      case "all":
+        results = await getAllProjects(offset);
+        break;
+      case "category":
+        if (!filter) {
+          return NextResponse.json({error: "Requesting projects of a certain category requires the category filter"}, {status: 404});
+        }
+        results = await getProjectsByCategory(filter!, offset);
+        break;
+      case "tech":
+        if (!filter) {
+          return NextResponse.json({error: "Requesting projects of a certain tech requires the tech filter"}, {status: 404});
+        }
+        results = await getProjectsByTech(filter!, offset);
+        break;
+      default:
+        return NextResponse.json({error: "Server cannot handle untyped request"}, {status: 500});
+    }
+
+    return NextResponse.json(results, {status: 200});
+  } catch (err) {
+    return NextResponse.json({error: "Internal server error"}, {status: 500});
   }
-  return NextResponse.json(results, {status: 200});
 }
